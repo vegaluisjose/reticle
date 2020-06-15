0a3c296698de920d1fb570a8e46f1ab5c51a04f7
Luis Vega <vegaluisjose@gmail.com>
2020-06-15 14:24:31 -0700

    add width=32 test

--- prim/ultrascale/test/test.v
+++ prim/ultrascale/test/test.v
@@ -18,8 +18,35 @@ module test_dsp_add_8 (
 
     always @(posedge clock) begin
         if (!reset && (cycles == 32'd0)) begin
-            assert (y == y_ref) $display ("[dsp_add] PASS");
-                else $error("[dsp_add] FAIL");
+            assert (y == y_ref) $display ("[test_dsp_add_8] PASS");
+                else $error("[test_dsp_add_8] FAIL");
+        end
+    end
+
+endmodule
+
+module test_dsp_add_32 (
+    input        clock,
+    input        reset,
+    input [31:0] cycles
+);
+    localparam width = 32;
+
+    logic [width-1:0] a;
+    logic [width-1:0] b;
+    logic [width-1:0] y;
+    logic [width-1:0] y_ref;
+
+    assign a = 32'd1;
+    assign b = 32'hffff0001;
+    assign y_ref = a + b;
+
+    dsp_add #(.width(width)) dut (clock, reset, a, b, y);
+
+    always @(posedge clock) begin
+        if (!reset && (cycles == 32'd0)) begin
+            assert (y == y_ref) $display ("[test_dsp_add_32] PASS");
+                else $error("[test_dsp_add_32] FAIL");
         end
     end
 
@@ -57,5 +84,6 @@ module test();
     end
 
     test_dsp_add_8 t0 (clock, reset, cycles);
+    test_dsp_add_32 t1 (clock, reset, cycles);
 
 endmodule
