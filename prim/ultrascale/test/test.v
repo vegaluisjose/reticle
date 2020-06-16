a65e495f980360b3577f02f17885dc3a7c028b56
Luis Vega <vegaluisjose@gmail.com>
2020-06-15 22:28:14 -0700

    add dsp xor ops

--- prim/ultrascale/test/test.v
+++ prim/ultrascale/test/test.v
@@ -670,6 +670,160 @@ module test_dsp_or_v3_width_12 (
 
 endmodule
 
+module test_dsp_xor_width_32 (
+    input        clock,
+    input        reset,
+    input [31:0] cycles
+);
+    localparam width = 32;
+
+    logic [width-1:0] a;
+    logic [width-1:0] b;
+    logic [width-1:0] y;
+    logic [width-1:0] y_ref;
+
+    assign a = 32'd10;
+    assign b = 32'd1;
+
+    assign y_ref = a ^ b;
+
+    dsp_xor #(.width(width)) dut (clock, reset, a, b, y);
+
+    always @(posedge clock) begin
+        if (!reset && (cycles == 32'd0)) begin
+            assert (y == y_ref) $display ("[PASS] test_dsp_xor_width_32");
+                else $error("[FAIL] test_dsp_xor_width_32");
+        end
+    end
+
+endmodule
+
+module test_dsp_xor_v2_width_24 (
+    input        clock,
+    input        reset,
+    input [31:0] cycles
+);
+    localparam width = 24;
+
+    logic [width-1:0] a0;
+    logic [width-1:0] b0;
+    logic [width-1:0] a1;
+    logic [width-1:0] b1;
+    logic [width-1:0] y0;
+    logic [width-1:0] y1;
+    logic [width-1:0] y0_ref;
+    logic [width-1:0] y1_ref;
+
+    assign a0 = -24'd1;
+    assign b0 = 24'd16;
+    assign a1 = 24'd23;
+    assign b1 = 24'd7;
+
+    assign y0_ref = a0 ^ b0;
+    assign y1_ref = a1 ^ b1;
+
+    dsp_xor_v2 #(.width(width)) dut (clock, reset, a0, b0, a1, b1, y0, y1);
+
+    always @(posedge clock) begin
+        if (!reset && (cycles == 32'd0)) begin
+            assert (y0 == y0_ref && y1 == y1_ref) $display ("[PASS] test_dsp_xor_v2_width_24");
+                else $error("[FAIL] test_dsp_xor_v2_width_24");
+        end
+    end
+
+endmodule
+
+module test_dsp_xor_v4_width_12 (
+    input        clock,
+    input        reset,
+    input [31:0] cycles
+);
+    localparam width = 12;
+
+    logic [width-1:0] a0;
+    logic [width-1:0] b0;
+    logic [width-1:0] a1;
+    logic [width-1:0] b1;
+    logic [width-1:0] a2;
+    logic [width-1:0] b2;
+    logic [width-1:0] a3;
+    logic [width-1:0] b3;
+    logic [width-1:0] y0;
+    logic [width-1:0] y1;
+    logic [width-1:0] y2;
+    logic [width-1:0] y3;
+    logic [width-1:0] y0_ref;
+    logic [width-1:0] y1_ref;
+    logic [width-1:0] y2_ref;
+    logic [width-1:0] y3_ref;
+
+    assign a0 = -12'd1;
+    assign b0 = 12'd16;
+    assign a1 = 12'd23;
+    assign b1 = 12'd7;
+    assign a2 = 12'd255;
+    assign b2 = 12'd7;
+    assign a3 = -12'd20;
+    assign b3 = -12'd7;
+
+    assign y0_ref = a0 ^ b0;
+    assign y1_ref = a1 ^ b1;
+    assign y2_ref = a2 ^ b2;
+    assign y3_ref = a3 ^ b3;
+
+    dsp_xor_v4 #(.width(width)) dut (clock, reset, a0, b0, a1, b1, a2, b2, a3, b3, y0, y1, y2, y3);
+
+    always @(posedge clock) begin
+        if (!reset && (cycles == 32'd0)) begin
+            assert (y0 == y0_ref && y1 == y1_ref && y2 == y2_ref && y3 == y3_ref) $display ("[PASS] test_dsp_xor_v4_width_12");
+                else $error("[FAIL] test_dsp_xor_v4_width_12");
+        end
+    end
+
+endmodule
+
+module test_dsp_xor_v3_width_12 (
+    input        clock,
+    input        reset,
+    input [31:0] cycles
+);
+    localparam width = 12;
+
+    logic [width-1:0] a0;
+    logic [width-1:0] b0;
+    logic [width-1:0] a1;
+    logic [width-1:0] b1;
+    logic [width-1:0] a2;
+    logic [width-1:0] b2;
+    logic [width-1:0] y0;
+    logic [width-1:0] y1;
+    logic [width-1:0] y2;
+    logic [width-1:0] y0_ref;
+    logic [width-1:0] y1_ref;
+    logic [width-1:0] y2_ref;
+
+    assign a0 = 12'd1;
+    assign b0 = 12'd16;
+    assign a1 = 12'd23;
+    assign b1 = 12'd7;
+    assign a2 = 12'd25;
+    assign b2 = 12'd7;
+
+    assign y0_ref = a0 ^ b0;
+    assign y1_ref = a1 ^ b1;
+    assign y2_ref = a2 ^ b2;
+
+    dsp_xor_v3 #(.width(width)) dut (clock, reset, a0, b0, a1, b1, a2, b2, y0, y1, y2);
+
+    always @(posedge clock) begin
+        if (!reset && (cycles == 32'd0)) begin
+            assert (y0 == y0_ref && y1 == y1_ref && y2 == y2_ref) $display ("[PASS] test_dsp_xor_v3_width_12");
+                else $error("[FAIL] test_dsp_xor_v3_width_12 ");
+        end
+    end
+
+endmodule
+
 module test();
     logic clock = 1'b0;
     logic reset;
@@ -719,5 +873,9 @@ module test();
     test_dsp_or_v2_width_24  t15 (clock, reset, cycles);
     test_dsp_or_v4_width_12  t16 (clock, reset, cycles);
     test_dsp_or_v3_width_12  t17 (clock, reset, cycles);
+    test_dsp_xor_width_32    t18 (clock, reset, cycles);
+    test_dsp_xor_v2_width_24 t19 (clock, reset, cycles);
+    test_dsp_xor_v4_width_12 t20 (clock, reset, cycles);
+    test_dsp_xor_v3_width_12 t21 (clock, reset, cycles);
 
 endmodule
