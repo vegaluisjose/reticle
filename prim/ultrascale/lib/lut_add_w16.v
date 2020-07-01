e7ee439b193d369f2b6c527ba1020dcd2def50ad
Luis Vega <vegaluisjose@gmail.com>
2020-06-30 19:43:34 -0700

    add lut-add-w16

---
+++ prim/ultrascale/lib/lut_add_w16.v
@@ -0,0 +1,64 @@
+module lut_add_w16 #
+(
+    parameter width = 16
+)
+(
+    input              clock,
+    input              reset,
+    input  [width-1:0] a,
+    input  [width-1:0] b,
+    output [width-1:0] y
+);
+
+    initial begin
+        assert(width == 8)
+            else $error("[lut_add_w16] width:%d configuration not supported", width);
+    end
+
+    wire zero;
+    wire [width-1:0] p;
+    wire [7:0] co;
+
+    GND GND(.G(zero));
+
+    (* LOC = "SLICE_X0Y0", BEL = "A6LUT" *)
+    LUT2 #(.INIT(4'h6)) l0 (.I0(a[0]), .I1(b[0]), .O(p[0]));
+    (* LOC = "SLICE_X0Y0", BEL = "B6LUT" *)
+    LUT2 #(.INIT(4'h6)) l1 (.I0(a[1]), .I1(b[1]), .O(p[1]));
+    (* LOC = "SLICE_X0Y0", BEL = "C6LUT" *)
+    LUT2 #(.INIT(4'h6)) l2 (.I0(a[2]), .I1(b[2]), .O(p[2]));
+    (* LOC = "SLICE_X0Y0", BEL = "D6LUT" *)
+    LUT2 #(.INIT(4'h6)) l3 (.I0(a[3]), .I1(b[3]), .O(p[3]));
+    (* LOC = "SLICE_X0Y0", BEL = "E6LUT" *)
+    LUT2 #(.INIT(4'h6)) l4 (.I0(a[4]), .I1(b[4]), .O(p[4]));
+    (* LOC = "SLICE_X0Y0", BEL = "F6LUT" *)
+    LUT2 #(.INIT(4'h6)) l5 (.I0(a[5]), .I1(b[5]), .O(p[5]));
+    (* LOC = "SLICE_X0Y0", BEL = "G6LUT" *)
+    LUT2 #(.INIT(4'h6)) l6 (.I0(a[6]), .I1(b[6]), .O(p[6]));
+    (* LOC = "SLICE_X0Y0", BEL = "H6LUT" *)
+    LUT2 #(.INIT(4'h6)) l7 (.I0(a[7]), .I1(b[7]), .O(p[7]));
+
+    (* LOC = "SLICE_X0Y0" *)
+    CARRY8 #(.CARRY_TYPE("SINGLE_CY8")) carry0 (.CI(zero), .CI_TOP(zero), .DI(a[7:0]), .S(p[7:0]), .O(y[7:0]), .CO(co));
+
+    (* LOC = "SLICE_X0Y1", BEL = "A6LUT" *)
+    LUT2 #(.INIT(4'h6)) l8 (.I0(a[8]), .I1(b[8]), .O(p[8]));
+    (* LOC = "SLICE_X0Y1", BEL = "B6LUT" *)
+    LUT2 #(.INIT(4'h6)) l9 (.I0(a[9]), .I1(b[9]), .O(p[9]));
+    (* LOC = "SLICE_X0Y1", BEL = "C6LUT" *)
+    LUT2 #(.INIT(4'h6)) l10 (.I0(a[10]), .I1(b[10]), .O(p[10]));
+    (* LOC = "SLICE_X0Y1", BEL = "D6LUT" *)
+    LUT2 #(.INIT(4'h6)) l11 (.I0(a[11]), .I1(b[11]), .O(p[11]));
+    (* LOC = "SLICE_X0Y1", BEL = "E6LUT" *)
+    LUT2 #(.INIT(4'h6)) l12 (.I0(a[12]), .I1(b[12]), .O(p[12]));
+    (* LOC = "SLICE_X0Y1", BEL = "F6LUT" *)
+    LUT2 #(.INIT(4'h6)) l13 (.I0(a[13]), .I1(b[13]), .O(p[13]));
+    (* LOC = "SLICE_X0Y1", BEL = "G6LUT" *)
+    LUT2 #(.INIT(4'h6)) l14 (.I0(a[14]), .I1(b[14]), .O(p[14]));
+    (* LOC = "SLICE_X0Y1", BEL = "H6LUT" *)
+    LUT2 #(.INIT(4'h6)) l15 (.I0(a[15]), .I1(b[15]), .O(p[15]));
+
+    (* LOC = "SLICE_X0Y1" *)
+    CARRY8 #(.CARRY_TYPE("SINGLE_CY8")) carry1 (.CI(co[7]), .CI_TOP(zero), .DI(a[15:8]), .S(p[15:8]), .O(y[15:8]), .CO());
+
+endmodule
--- prim/ultrascale/test/test.v
+++ prim/ultrascale/test/test.v
@@ -612,6 +612,34 @@ module test_lut_sub_width_8 (
 
 endmodule
 
+module test_lut_add_width_16 (
+    input        clock,
+    input        reset,
+    input [31:0] cycles
+);
+    localparam width = 16;
+
+    logic [width-1:0] a;
+    logic [width-1:0] b;
+    logic [width-1:0] y;
+    logic [width-1:0] y_ref;
+
+    assign a = 16'd1024;
+    assign b = 16'd100;
+
+    assign y_ref = a + b;
+
+    lut_add_w16 #(.width(width)) dut (clock, reset, a, b, y);
+
+    always @(posedge clock) begin
+        if (!reset && (cycles == 32'd0)) begin
+            assert (y == y_ref) $display ("[PASS] test_lut_add_width_16");
+                else $error("[FAIL] test_lut_add_width_16");
+        end
+    end
+
+endmodule
+
 module test();
     logic clock = 1'b0;
     logic reset;
@@ -662,5 +690,6 @@ module test();
     test_dsp_not_width_32    t16 (clock, reset, cycles);
     test_lut_add_width_8     t17 (clock, reset, cycles);
     test_lut_sub_width_8     t18 (clock, reset, cycles);
+    test_lut_add_width_16    t19 (clock, reset, cycles);
 
 endmodule
