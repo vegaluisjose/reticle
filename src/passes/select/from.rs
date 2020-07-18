c094b4b4e774c56d8f20961109ead8f41d302aba
Luis Vega <vegaluisjose@gmail.com>
2020-07-17 17:34:25 -0700

    add the init prototype

--- src/passes/select/from.rs
+++ src/passes/select/from.rs
@@ -2,8 +2,19 @@ use std::convert::From;
 use crate::lang::ast;
 use crate::passes::select::dag::*;
 
+impl From<ast::Def> for SDag {
+    fn from(def: ast::Def) -> Self {
+        let sdag = SDag::new();
+        for instr in def.body.iter() {
+            println!("{}", instr);
+        }
+        sdag
+    }
+}
+
 impl From<ast::Prog> for SDag {
-    fn from(program: ast::Prog) -> Self {
-        SDag::new()
+    fn from(prog: ast::Prog) -> Self {
+        assert!(prog.defs.len() == 1, "Error: only single definition allowed atm");
+        SDag::from(prog.defs[0].clone())
     }
 }
