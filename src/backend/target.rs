97ca8c708890b0ac5ff621f83bfb71826825674b
Luis Vega <vegaluisjose@gmail.com>
2020-07-19 12:21:10 -0700

    rename select import and add tile type

--- src/backend/target.rs
+++ src/backend/target.rs
@@ -1,4 +1,5 @@
-use crate::passes::select::instr;
+use crate::passes::select::instr as sel;
+use crate::backend::asm;
 use serde::{Deserialize, Serialize};
 use serde_json;
 use std::rc::Rc;
@@ -27,27 +28,33 @@ pub struct Def {
 }
 
 #[derive(Clone, Debug)]
+pub struct Tile {
+    pub asm: asm::Instr,
+    pub pattern: sel::Pattern,
+}
+
+#[derive(Clone, Debug)]
 pub struct Target {
-    pub patterns: Vec<instr::Pattern>,
+    pub patterns: Vec<sel::Pattern>,
 }
 
 impl Expr {
     pub fn to_instr_mut(
         &self,
-        instr: &mut Vec<instr::Instr>,
-        op_ty: instr::Ty,
-        op_loc: instr::Loc,
+        instr: &mut Vec<sel::Instr>,
+        op_ty: sel::Ty,
+        op_loc: sel::Loc,
     ) {
         match self {
             Expr::Input(ty, loc) => {
-                let op = instr::Op::In;
-                let ty = instr::Ty::from_str(ty).unwrap();
-                let inp_loc = instr::Loc::from_str(loc).unwrap();
-                instr.push(instr::Instr::new(op, ty, inp_loc));
+                let op = sel::Op::In;
+                let ty = sel::Ty::from_str(ty).unwrap();
+                let inp_loc = sel::Loc::from_str(loc).unwrap();
+                instr.push(sel::Instr::new(op, ty, inp_loc));
             }
             Expr::BinOp(op, lhs, rhs) => {
-                let op = instr::Op::from_str(op).unwrap();
-                instr.push(instr::Instr::new(op, op_ty.clone(), op_loc.clone()));
+                let op = sel::Op::from_str(op).unwrap();
+                instr.push(sel::Instr::new(op, op_ty.clone(), op_loc.clone()));
                 lhs.to_instr_mut(instr, op_ty.clone(), op_loc.clone());
                 rhs.to_instr_mut(instr, op_ty.clone(), op_loc.clone());
             }
@@ -56,12 +63,12 @@ impl Expr {
 }
 
 impl Instr {
-    fn to_pattern(&self) -> instr::Pattern {
-        let ty = instr::Ty::from_str(&self.ty).unwrap();
-        let loc = instr::Loc::from_str(&self.loc).unwrap();
-        let mut instr: Vec<instr::Instr> = Vec::new();
+    fn to_pattern(&self) -> sel::Pattern {
+        let ty = sel::Ty::from_str(&self.ty).unwrap();
+        let loc = sel::Loc::from_str(&self.loc).unwrap();
+        let mut instr: Vec<sel::Instr> = Vec::new();
         self.expr.to_instr_mut(&mut instr, ty, loc);
-        instr::Pattern {
+        sel::Pattern {
             name: self.name.to_string(),
             cost: self.cost.clone(),
             instr: instr.to_vec(),
@@ -71,7 +78,7 @@ impl Instr {
 
 impl From<Def> for Target {
     fn from(def: Def) -> Self {
-        let mut patterns: Vec<instr::Pattern> = Vec::new();
+        let mut patterns: Vec<sel::Pattern> = Vec::new();
         for instr in def.instr.iter() {
             patterns.push(instr.to_pattern());
         }
