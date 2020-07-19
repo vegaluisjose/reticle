2126507be4af0af3b0ae202951538bc7d8c17a4d
Luis Vega <vegaluisjose@gmail.com>
2020-07-19 12:12:47 -0700

    add asm file

---
+++ src/backend/asm.rs
@@ -0,0 +1,41 @@
+use crate::lang::ast;
+use std::collections::HashMap;
+
+pub type Id = ast::Id;
+pub type Ty = ast::Ty;
+pub type LocTy = ast::Loc;
+
+#[derive(Clone, Debug)]
+pub struct Loc {
+    ty: LocTy,
+    x: u32,
+    y: u32,
+}
+
+#[derive(Clone, Debug)]
+pub struct Instr {
+    pub id: Id,
+    pub ty: Ty,
+    pub op: Id,
+    pub loc: Loc,
+    pub params: Vec<Id>,
+}
+
+#[derive(Clone, Debug)]
+pub enum Port {
+    Input { id: Id, ty: Ty },
+    Output { id: Id, ty: Ty },
+}
+
+#[derive(Clone, Debug)]
+pub struct Sig {
+    pub id: Id,
+    pub inputs: Vec<Port>,
+    pub outputs: Vec<Port>,
+}
+
+#[derive(Clone, Debug)]
+pub struct Prog {
+    pub sig: Sig,
+    pub body: Vec<Instr>,
+}
