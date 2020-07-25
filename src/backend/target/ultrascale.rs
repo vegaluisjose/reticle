8c5d57c7df41eefd191c09a27a5831fbea9b6962
Luis Vega <vegaluisjose@gmail.com>
2020-07-24 21:08:32 -0700

    add method to retrieve spec

--- src/backend/target/ultrascale.rs
+++ src/backend/target/ultrascale.rs
@@ -7,16 +7,22 @@ pub struct Ultrascale {
     pub spec: String,
 }
 
-impl Target for Ultrascale {
-    fn to_descriptor(&self) -> Descriptor {
-        Descriptor::from_str(&self.spec).unwrap()
-    }
-}
-
 impl Default for Ultrascale {
     fn default() -> Self {
         Ultrascale {
             spec: read_to_string("isa/ultrascale.json"),
         }
+    }
+}
+
+impl Ultrascale {
+    fn spec(&self) -> String {
+        self.spec.to_string()
+    }
+}
+
+impl Target for Ultrascale {
+    fn to_descriptor(&self) -> Descriptor {
+        Descriptor::from_str(&self.spec()).unwrap()
     }
 }
