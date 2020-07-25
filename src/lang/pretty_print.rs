2c525ab13bc4e73f3c50db3b1d0f6b2c2d301733
Luis Vega <vegaluisjose@gmail.com>
2020-07-24 21:02:23 -0700

    bugfix params pretty printing

--- src/lang/pretty_print.rs
+++ src/lang/pretty_print.rs
@@ -85,7 +85,7 @@ impl PrettyPrint for Instr {
                         ))
                         .append(RcDoc::text("]"))
                 };
-                let params_doc = if !params.is_empty() {
+                let params_doc = if params.is_empty() {
                     RcDoc::nil()
                 } else {
                     RcDoc::text("(")
