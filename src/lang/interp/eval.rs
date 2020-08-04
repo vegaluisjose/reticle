488a1d00946a5f7ef8d5d77cd37da9d810fc73e5
Luis Vega <vegaluisjose@gmail.com>
2020-08-04 09:06:12 -0700

    add more ops to eval

--- src/lang/interp/eval.rs
+++ src/lang/interp/eval.rs
@@ -20,12 +20,27 @@ impl Eval for Instr {
             Instr::Prim {
                 id: _,
                 ty: _,
-                op: PrimOp::Reg,
+                op: PrimOp::Not,
+                attrs: _,
+                params,
+                loc: _,
+            } => state.contains(&params[0].id()),
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: PrimOp::Mux,
+                attrs: _,
+                params,
+                loc: _,
+            } => state.contains(&params[0].id()) && state.contains(&params[1].id() && state.contains(&params[2].id()),
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: _,
                 attrs: _,
                 params,
                 loc: _,
             } => state.contains(&params[0].id()) && state.contains(&params[1].id()),
-            instr => unimplemented!("instr:{} not supported yet", instr),
         }
     }
 
@@ -53,7 +68,149 @@ impl Eval for Instr {
                     state.get(id)
                 }
             }
-            instr => unimplemented!("instr: {} not supported yet", instr),
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: PrimOp::Add,
+                attrs: _,
+                params,
+                loc: _,
+            } => state.get(&params[0].id()) + state.get(&params[1].id()),
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: PrimOp::Sub,
+                attrs: _,
+                params,
+                loc: _,
+            } => state.get(&params[0].id()) - state.get(&params[1].id()),
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: PrimOp::Mul,
+                attrs: _,
+                params,
+                loc: _,
+            } => state.get(&params[0].id()) * state.get(&params[1].id()),
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: PrimOp::Not,
+                attrs: _,
+                params,
+                loc: _,
+            } => !state.get(&params[0].id()),
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: PrimOp::And,
+                attrs: _,
+                params,
+                loc: _,
+            } => state.get(&params[0].id()) & state.get(&params[1].id()),
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: PrimOp::Nand,
+                attrs: _,
+                params,
+                loc: _,
+            } => !(state.get(&params[0].id()) & state.get(&params[1].id())),
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: PrimOp::Or,
+                attrs: _,
+                params,
+                loc: _,
+            } => state.get(&params[0].id()) | state.get(&params[1].id()),
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: PrimOp::Nor,
+                attrs: _,
+                params,
+                loc: _,
+            } => !(state.get(&params[0].id()) | state.get(&params[1].id())),
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: PrimOp::Xor,
+                attrs: _,
+                params,
+                loc: _,
+            } => state.get(&params[0].id()) ^ state.get(&params[1].id()),
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: PrimOp::Xnor,
+                attrs: _,
+                params,
+                loc: _,
+            } => !(state.get(&params[0].id()) ^ state.get(&params[1].id())),
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: PrimOp::Mux,
+                attrs: _,
+                params,
+                loc: _,
+            } => {
+                let en = state.get(&params[0].id());
+                if en > 0 {
+                    state.get(&params[1].id())
+                } else {
+                    state.get(&params[2].id())
+                }
+            }
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: PrimOp::Equal,
+                attrs: _,
+                params,
+                loc: _,
+            } => (state.get(&params[0].id()) == state.get(&params[1].id())) as i64,
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: PrimOp::Nequal,
+                attrs: _,
+                params,
+                loc: _,
+            } => (state.get(&params[0].id()) != state.get(&params[1].id())) as i64,
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: PrimOp::Gt,
+                attrs: _,
+                params,
+                loc: _,
+            } => (state.get(&params[0].id()) > state.get(&params[1].id())) as i64,
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: PrimOp::Lt,
+                attrs: _,
+                params,
+                loc: _,
+            } => (state.get(&params[0].id()) < state.get(&params[1].id())) as i64,
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: PrimOp::Ge,
+                attrs: _,
+                params,
+                loc: _,
+            } => (state.get(&params[0].id()) >= state.get(&params[1].id())) as i64,
+            Instr::Prim {
+                id: _,
+                ty: _,
+                op: PrimOp::Le,
+                attrs: _,
+                params,
+                loc: _,
+            } => (state.get(&params[0].id()) <= state.get(&params[1].id())) as i64,
         }
     }
 }
