602598fea4f7d8945e2827587451dadccb348713
Luis Vega <vegaluisjose@gmail.com>
2020-08-07 12:12:46 -0700

    move files to frontend

--- src/frontend/mod.rs
+++ src/frontend/mod.rs
@@ -1,2 +1,5 @@
+pub mod display;
+pub mod from_str;
 pub mod parser;
+pub mod pretty_print;
 pub mod type_check;
--- src/lang/mod.rs
+++ src/lang/mod.rs
@@ -1,7 +1,3 @@
 pub mod ast;
 pub mod default;
-pub mod display;
-pub mod dot;
-pub mod from_str;
 pub mod helpers;
-pub mod pretty_print;
