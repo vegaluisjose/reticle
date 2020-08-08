572f0a0b869cfdd1e751805c7bdeec7c7b108818
Luis Vega <vegaluisjose@gmail.com>
2020-08-07 17:23:37 -0700

    add a counter test

--- tests/basic.rs
+++ tests/basic.rs
@@ -38,4 +38,17 @@ mod tests {
         trace.enq("y", 11);
         assert!(!Interpreter::default().run(&prog, &trace).is_failed());
     }
+
+    #[test]
+    fn test_counter() {
+        let prog = parse_from_file("examples/basic/counter.ret");
+        let mut trace = Trace::default();
+        trace.enq("en", 1);
+        trace.enq("y", 0);
+        trace.enq("en", 1);
+        trace.enq("y", 1);
+        trace.enq("en", 0);
+        trace.enq("y", 2);
+        assert!(!Interpreter::default().run(&prog, &trace).is_failed());
+    }
 }
