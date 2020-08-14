01171e776369562b1ecdac71596b2ed521a36624
Luis Vega <vegaluisjose@gmail.com>
2020-08-14 09:33:23 -0700

    add remaining basic test back

--- tests/basic.rs
+++ tests/basic.rs
@@ -18,73 +18,81 @@ mod test_basic {
             .run(&prog, &trace)
             .is_failed());
     }
+
+    #[test]
+    fn test_pipeline() {
+        let prog = parse_from_file("examples/basic/pipeline.ret");
+        let mut trace = Trace::default();
+        trace.enq("a", Value::new_scalar(9));
+        trace.enq("a", Value::new_scalar(0));
+        trace.enq("a", Value::new_scalar(0));
+        trace.enq("en", Value::new_scalar(1));
+        trace.enq("en", Value::new_scalar(1));
+        trace.enq("en", Value::new_scalar(0));
+        trace.enq("y", Value::new_scalar(0));
+        trace.enq("y", Value::new_scalar(0));
+        trace.enq("y", Value::new_scalar(9));
+        assert!(!Interpreter::default()
+            .with_print()
+            .run(&prog, &trace)
+            .is_failed());
+    }
+
+    #[test]
+    fn test_muladd() {
+        let prog = parse_from_file("examples/basic/muladd.ret");
+        let mut trace = Trace::default();
+        trace.enq("a", Value::new_scalar(4));
+        trace.enq("a", Value::new_scalar(0));
+        trace.enq("b", Value::new_scalar(2));
+        trace.enq("b", Value::new_scalar(0));
+        trace.enq("c", Value::new_scalar(3));
+        trace.enq("c", Value::new_scalar(3));
+        trace.enq("en", Value::new_scalar(1));
+        trace.enq("en", Value::new_scalar(0));
+        trace.enq("y", Value::new_scalar(3));
+        trace.enq("y", Value::new_scalar(11));
+        assert!(!Interpreter::default()
+            .with_print()
+            .run(&prog, &trace)
+            .is_failed());
+    }
+
+    #[test]
+    fn test_counter() {
+        let prog = parse_from_file("examples/basic/counter.ret");
+        let mut trace = Trace::default();
+        trace.enq("en", Value::new_scalar(1));
+        trace.enq("en", Value::new_scalar(1));
+        trace.enq("en", Value::new_scalar(0));
+        trace.enq("y", Value::new_scalar(0));
+        trace.enq("y", Value::new_scalar(1));
+        trace.enq("y", Value::new_scalar(2));
+        assert!(!Interpreter::default()
+            .with_print()
+            .run(&prog, &trace)
+            .is_failed());
+    }
+
+    #[test]
+    fn test_fsm_two() {
+        let prog = parse_from_file("examples/basic/fsm_two.ret");
+        let mut trace = Trace::default();
+        trace.enq("start", Value::new_scalar(0));
+        trace.enq("start", Value::new_scalar(1));
+        trace.enq("start", Value::new_scalar(0));
+        trace.enq("start", Value::new_scalar(0));
+        trace.enq("done", Value::new_scalar(0));
+        trace.enq("done", Value::new_scalar(0));
+        trace.enq("done", Value::new_scalar(1));
+        trace.enq("done", Value::new_scalar(0));
+        trace.enq("state", Value::new_scalar(0));
+        trace.enq("state", Value::new_scalar(0));
+        trace.enq("state", Value::new_scalar(1));
+        trace.enq("state", Value::new_scalar(0));
+        assert!(!Interpreter::default()
+            .with_print()
+            .run(&prog, &trace)
+            .is_failed());
+    }
 }
-
-//     #[test]
-//     fn test_pipeline() {
-//         let prog = parse_from_file("examples/basic/pipeline.ret");
-//         let mut trace = Trace::default();
-//         trace.enq("a", 9);
-//         trace.enq("en", 1);
-//         trace.enq("y", 0);
-//         trace.enq("a", 0);
-//         trace.enq("en", 1);
-//         trace.enq("y", 0);
-//         trace.enq("a", 0);
-//         trace.enq("en", 0);
-//         trace.enq("y", 9);
-//         assert!(!Interpreter::default().run(&prog, &trace).is_failed());
-//     }
-
-//     #[test]
-//     fn test_muladd() {
-//         let prog = parse_from_file("examples/basic/muladd.ret");
-//         let mut trace = Trace::default();
-//         trace.enq("a", 4);
-//         trace.enq("b", 2);
-//         trace.enq("c", 3);
-//         trace.enq("en", 1);
-//         trace.enq("y", 3);
-//         trace.enq("a", 0);
-//         trace.enq("b", 0);
-//         trace.enq("c", 3);
-//         trace.enq("en", 0);
-//         trace.enq("y", 11);
-//         assert!(!Interpreter::default().run(&prog, &trace).is_failed());
-//     }
-
-//     #[test]
-//     fn test_counter() {
-//         let prog = parse_from_file("examples/basic/counter.ret");
-//         let mut trace = Trace::default();
-//         trace.enq("en", 1);
-//         trace.enq("y", 0);
-//         trace.enq("en", 1);
-//         trace.enq("y", 1);
-//         trace.enq("en", 0);
-//         trace.enq("y", 2);
-//         assert!(!Interpreter::default().run(&prog, &trace).is_failed());
-//     }
-
-//     #[test]
-//     fn test_fsm_two() {
-//         let prog = parse_from_file("examples/basic/fsm_two.ret");
-//         let mut trace = Trace::default();
-//         trace.enq("start", 0);
-//         trace.enq("start", 1);
-//         trace.enq("start", 0);
-//         trace.enq("start", 0);
-//         trace.enq("done", 0);
-//         trace.enq("done", 0);
-//         trace.enq("done", 1);
-//         trace.enq("done", 0);
-//         trace.enq("state", 0);
-//         trace.enq("state", 0);
-//         trace.enq("state", 1);
-//         trace.enq("state", 0);
-//         assert!(!Interpreter::default()
-//             .with_print()
-//             .run(&prog, &trace)
-//             .is_failed());
-//     }
-// }
