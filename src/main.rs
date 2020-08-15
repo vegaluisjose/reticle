fc222b7fb7fa16f01953d61f1568db5df97348fe
Luis Vega <vegaluisjose@gmail.com>
2020-08-14 22:38:28 -0700

    start working on the second iteration of mapping

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,7 @@
-// // compilation pipeline example
-// use reticle::frontend::parser::parse_from_file;
-// use reticle::passes::select::Select;
-// fn main() {
-//     let prog = parse_from_file("examples/basic/muladd.ret");
-//     let pass = Select::new(prog.clone(), "ultrascale");
-//     let asm = pass.run();
-//     let vlog = asm.to_verilog();
-//     println!("\n\nOriginal program:\n\n{}", &prog);
-//     println!("\n\nAsm program:\n\n{}", &asm);
-//     println!("\n\nVerilog module:\n\n{}", vlog);
-// }
-
-use reticle::interp::trace::Trace;
-use reticle::interp::ty::Value;
+use reticle::frontend::parser::parse_from_file;
+use reticle::passes::map::example;
 
 fn main() {
-    let mut trace = Trace::default();
-    trace.enq("a", Value::new_scalar(4));
-    let x = Value::new_scalar(2);
-    let mut y = Value::new_vector();
-    y.push(9);
-    y.push(0);
-    println!("x:{}", x);
-    println!("y:{}", y);
+    let prog = parse_from_file("examples/basic/fsm.ret");
+    example(&prog);
 }
---
+++ src/passes/map/mod.rs
@@ -0,0 +1,178 @@
+use crate::lang::ast::{Instr, Port, Prog};
+use petgraph::dot::{Config, Dot};
+use petgraph::graph::NodeIndex;
+use petgraph::prelude::Graph;
+use std::collections::HashMap;
+use std::fmt;
+
+#[derive(Clone, Debug)]
+pub enum DagNodeValue {
+    Inp(Port),
+    Out(Port),
+    Ins(Instr),
+}
+
+#[derive(Clone, Debug)]
+pub struct DagNode {
+    pub value: DagNodeValue,
+    pub visited: bool,
+    pub root: bool,
+}
+
+#[derive(Default, Clone, Debug)]
+pub struct DagEdge;
+
+pub type DGraph = Graph<DagNode, DagEdge>;
+pub type DagIx = NodeIndex;
+pub type DagId = String;
+pub type DagCtx = HashMap<DagId, DagIx>;
+
+#[derive(Clone, Debug)]
+pub struct Dag {
+    pub dag: DGraph,
+    pub ctx: DagCtx,
+}
+
+impl From<Port> for DagNodeValue {
+    fn from(port: Port) -> Self {
+        match port {
+            Port::Input {..} => DagNodeValue::Inp(port),
+            Port::Output {..} => DagNodeValue::Out(port),
+        }
+    }
+}
+
+impl From<Instr> for DagNodeValue {
+    fn from(instr: Instr) -> Self {
+        DagNodeValue::Ins(instr)
+    }
+}
+
+impl DagNodeValue {
+    pub fn id(&self) -> String {
+        match self {
+            DagNodeValue::Inp(port) => port.id(),
+            DagNodeValue::Out(port) => port.id(),
+            DagNodeValue::Ins(instr) => instr.id(),
+        }
+    }
+
+    pub fn op(&self) -> String {
+        match self {
+            DagNodeValue::Inp(_) => "input".to_string(),
+            DagNodeValue::Out(_) => "output".to_string(),
+            DagNodeValue::Ins(instr) => {
+                if instr.is_std() {
+                    instr.std_op().to_string()
+                } else {
+                    instr.prim_op().to_string()
+                }
+            }
+        }
+    }
+}
+
+impl DagNode {
+    pub fn new(value: DagNodeValue) -> DagNode {
+        DagNode {
+            value,
+            visited: false,
+            root: false,
+        }
+    }
+
+    pub fn is_visited(&self) -> bool {
+        self.visited
+    }
+
+    pub fn is_root(&self) -> bool {
+        self.root
+    }
+
+    pub fn set_visited(&mut self) {
+        self.visited = true;
+    }
+
+    pub fn set_root(&mut self) {
+        self.root = true;
+    }
+}
+
+impl Default for Dag {
+    fn default() -> Dag {
+        Dag {
+            dag: DGraph::new(),
+            ctx: DagCtx::new(),
+        }
+    }
+}
+
+impl Dag {
+    pub fn add_node(&mut self, name: &str, value: DagNodeValue) {
+        let ix = self.dag.add_node(DagNode::new(value));
+        self.ctx.insert(name.to_string(), ix);
+    }
+
+    pub fn contains_node(&self, name: &str) -> bool {
+        self.ctx.contains_key(name)
+    }
+
+    pub fn add_edge(&mut self, from: &str, to: &str) {
+        if let Some(from_ix) = self.ctx.get(from) {
+            if let Some(to_ix) = self.ctx.get(to) {
+                if self.dag.find_edge(*from_ix, *to_ix).is_none() {
+                    self.dag.add_edge(*from_ix, *to_ix, DagEdge::default());
+                }
+            }
+        }
+    }
+}
+
+impl fmt::Display for DagNode {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}:{}", self.value.id(), self.value.op())
+    }
+}
+
+impl fmt::Display for DagEdge {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "")
+    }
+}
+
+impl fmt::Display for Dag {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", Dot::with_config(&self.dag, &[Config::EdgeNoLabel]))
+    }
+}
+
+pub fn example(prog: &Prog) {
+    let mut dag = Dag::default();
+    if let Some(def) = prog.defs().iter().next() {
+        for input in def.inputs().iter() {
+            if !dag.contains_node(&input.id()) {
+                let val = DagNodeValue::from(input.clone());
+                dag.add_node(&input.id(), val);
+            }
+        }
+        for output in def.outputs().iter() {
+            if !dag.contains_node(&output.id()) {
+                let val = DagNodeValue::from(output.clone());
+                dag.add_node(&output.id(), val);
+            }
+        }
+        for instr in def.body().iter() {
+            if !dag.contains_node(&instr.id()) {
+                let val = DagNodeValue::from(instr.clone());
+                dag.add_node(&instr.id(), val);
+            }
+        }
+        for instr in def.body().iter() {
+            for param in instr.params().iter() {
+                dag.add_edge(&param.id(), &instr.id());
+            }
+        }
+        println!("{}", dag);
+        println!("{}", prog);
+    }
+}
