c12f57ce4b887f9160df39c281bb64236755bcc9
Luis Vega <vegaluisjose@gmail.com>
2020-08-23 21:15:29 -0700

    add more checks to add instr

--- src/frontend/check.rs
+++ src/frontend/check.rs
@@ -11,17 +11,24 @@ impl Check for Instr {
     fn check(&self) {
         match self {
             Instr::Prim {
-                id: _,
+                id,
                 ty,
                 op: PrimOp::Add,
-                attrs: _,
+                attrs,
                 params,
                 loc: _,
             } => {
                 assert!(
                     ty == params[0].ty(),
-                    "Error: dst and param[0] does not match"
+                    "Error: [add] result type and param[0] type must match"
                 );
+                assert!(
+                    ty == params[1].ty(),
+                    "Error: [add] result type and param[1] type must match"
+                );
+                assert!(params.len() == 2, "Error: [add] support only two params");
+                assert!(attrs.is_empty(), "Error: [add] does not support attr");
+                assert!(!id.is_empty(), "Error: [add] must have an id");
             }
             _ => (),
         }
@@ -32,7 +39,7 @@ impl Check for Prog {
     fn check(&self) {
         assert!(
             self.defs().len() == 1,
-            "Error support only one definition atm"
+            "Error: support only one definition atm"
         );
         if let Some(def) = self.defs().iter().next() {
             for instr in def.body().iter() {
