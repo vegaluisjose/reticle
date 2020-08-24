8d7a997d8a6b6c755162409fc20cf1e61c0212e8
Luis Vega <vegaluisjose@gmail.com>
2020-08-24 12:43:05 -0700

    remove loc field from tree node

--- src/passes/map/tree/helpers.rs
+++ src/passes/map/tree/helpers.rs
@@ -9,7 +9,6 @@ impl TreeNode {
             id: id.to_string(),
             ty,
             op,
-            loc: TreeLoc::Hole,
             matched: false,
             tile: None,
             cost: f32::INFINITY,
@@ -21,7 +20,6 @@ impl TreeNode {
             id: id.to_string(),
             ty,
             op,
-            loc: TreeLoc::Hole,
             matched: false,
             tile: None,
             cost,
@@ -33,7 +31,6 @@ impl TreeNode {
             id: id.to_string(),
             ty,
             op: TreeOp::Input,
-            loc: TreeLoc::Hole,
             matched: false,
             tile: None,
             cost: 0 as f32,
@@ -50,10 +47,6 @@ impl TreeNode {
 
     pub fn op(&self) -> &TreeOp {
         &self.op
-    }
-
-    pub fn loc(&self) -> &TreeLoc {
-        &self.loc
     }
 
     pub fn instr(&self) -> Option<&Instr> {
@@ -74,10 +67,6 @@ impl TreeNode {
 
     pub fn set_matched(&mut self) {
         self.matched = true;
-    }
-
-    pub fn set_loc(&mut self, loc: TreeLoc) {
-        self.loc = loc;
     }
 
     pub fn set_tile(&mut self, tile: Tile) {
