885ba644ce2f8900182a347c1e33dd4c615eee69
Luis Vega <vegaluisjose@gmail.com>
2020-08-26 11:16:26 -0700

    refactor running demo main

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-use reticle::backend::asm::verilog::ToVerilog;
-use reticle::frontend::parser::parse_from_file;
-use reticle::passes::map::{map_analysis, map_asm, map_clear, map_loc};
-
-fn main() {
+pub fn compiler_example() {
+    use reticle::backend::asm::verilog::ToVerilog;
+    use reticle::frontend::parser::parse_from_file;
+    use reticle::passes::map::{map_analysis, map_asm, map_clear, map_loc};
     let prog = parse_from_file("examples/basic/fsm.ret");
     let prog_with_loc = map_loc(prog.clone());
     let analysis = map_analysis(prog_with_loc.clone());
@@ -16,13 +15,17 @@ fn main() {
     println!("\n---verilog---\n{}", verilog);
 }
 
-// use reticle::backend::arch::ultrascale::prim::lut::Lut;
+pub fn lut_example() {
+    use reticle::backend::arch::ultrascale::lut::Lut;
+    let mut lut2 = Lut::new_lut2("i0");
+    lut2.add_input("a0");
+    lut2.add_input("b0");
+    lut2.set_output("y");
+    lut2.set_init(6);
+    println!("{}", lut2);
+}
 
-// pub fn main() {
-//     let mut lut2 = Lut::new_lut2("i0");
-//     lut2.add_input("a0");
-//     lut2.add_input("b0");
-//     lut2.set_output("y");
-//     lut2.set_init(6);
-//     println!("{}", lut2);
-// }
+fn main() {
+    compiler_example();
+    lut_example();
+}
