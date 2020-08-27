8a89559ca925e4022535f28fbcbccc2bca71aa2a
Luis Vega <vegaluisjose@gmail.com>
2020-08-27 16:13:04 -0700

    fix fmt

--- src/backend/arch/ultrascale/lut/from.rs
+++ src/backend/arch/ultrascale/lut/from.rs
@@ -15,10 +15,7 @@ impl From<LutPrim> for verilog::Stmt {
     fn from(lut: LutPrim) -> Self {
         let mut inst = verilog::Instance::new(&lut.id(), &lut.ty().to_string());
         let width = lut_width(lut.ty().clone());
-        inst.add_param(
-            "INIT",
-            verilog::Expr::new_ulit_dec(width, &lut.init()),
-        );
+        inst.add_param("INIT", verilog::Expr::new_ulit_dec(width, &lut.init()));
         for (i, input) in lut.inputs().iter().enumerate() {
             let port = format!("I{}", i);
             inst.connect(&port, verilog::Expr::new_ref(input));
