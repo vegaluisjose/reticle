c00cc159077315969d75745e2960eb5c5f289551
Luis Vega <vegaluisjose@gmail.com>
2020-08-29 16:09:52 -0700

    finish impl LutEqI8I8

--- src/backend/arch/ultrascale/isa/lut_eq_b_i8_i8.rs
+++ src/backend/arch/ultrascale/isa/lut_eq_b_i8_i8.rs
@@ -44,11 +44,15 @@ use crate::backend::verilog;
 pub struct LutEqI8I8;
 
 impl EmitPrim for LutEqI8I8 {
-    fn emit_prim(asm: &mut Assembler, _: asm::InstrPrim) {
-        // let params: Vec<String> = instr.params().iter().map(|x| x.id()).collect();
-        // let lhs = asm.fresh_variable(&params[0]);
-        // let rhs = asm.fresh_variable(&params[1]);
-        // let res = asm.fresh_variable(&instr.dst_id());
+    fn emit_prim(asm: &mut Assembler, instr: asm::InstrPrim) {
+        let params: Vec<String> = instr.params().iter().map(|x| x.id()).collect();
+        let lhs = asm.fresh_variable(&params[0]);
+        let rhs = asm.fresh_variable(&params[1]);
+        let res = asm.fresh_variable(&instr.dst_id());
+        let wire_0_name = asm.new_variable_name();
+        let wire_1_name = asm.new_variable_name();
+        let wire_0 = verilog::Decl::new_wire(&wire_0_name, 1);
+        let wire_1 = verilog::Decl::new_wire(&wire_1_name, 1);
         let mut lut_0 = LutPrim::new_lut6();
         let mut lut_1 = LutPrim::new_lut6();
         let mut lut_2 = LutPrim::new_lut6();
@@ -58,6 +62,29 @@ impl EmitPrim for LutEqI8I8 {
         lut_0.set_init("9009000000000000");
         lut_1.set_init("9009000000009009");
         lut_2.set_init("9009000000009009");
+        lut_0.add_input_with_index(&rhs, 7);
+        lut_0.add_input_with_index(&lhs, 7);
+        lut_0.add_input_with_index(&rhs, 6);
+        lut_0.add_input_with_index(&lhs, 6);
+        lut_0.add_input(&wire_0_name);
+        lut_0.add_input(&wire_1_name);
+        lut_0.set_output(&res);
+        lut_1.add_input_with_index(&lhs, 3);
+        lut_1.add_input_with_index(&rhs, 3);
+        lut_1.add_input_with_index(&rhs, 5);
+        lut_1.add_input_with_index(&lhs, 5);
+        lut_1.add_input_with_index(&rhs, 4);
+        lut_1.add_input_with_index(&lhs, 4);
+        lut_1.set_output(&wire_0_name);
+        lut_2.add_input_with_index(&lhs, 0);
+        lut_2.add_input_with_index(&rhs, 0);
+        lut_2.add_input_with_index(&rhs, 2);
+        lut_2.add_input_with_index(&lhs, 2);
+        lut_2.add_input_with_index(&rhs, 1);
+        lut_2.add_input_with_index(&lhs, 1);
+        lut_2.set_output(&wire_1_name);
+        asm.add_wire(verilog::Stmt::from(wire_0));
+        asm.add_wire(verilog::Stmt::from(wire_1));
         asm.add_lut(verilog::Stmt::from(lut_0));
         asm.add_lut(verilog::Stmt::from(lut_1));
         asm.add_lut(verilog::Stmt::from(lut_2));
