cb86128d8162aed515a445e741ddc6a8a492a763
Luis Vega <vegaluisjose@gmail.com>
2020-08-31 08:35:30 -0700

    use a bigger wire instead of multiple bit-level wires

--- src/backend/arch/ultrascale/isa/lut_reg_mux_i8_b_i8_i8_b.rs
+++ src/backend/arch/ultrascale/isa/lut_reg_mux_i8_b_i8_i8_b.rs
@@ -14,26 +14,26 @@ impl EmitPrim for LutRegMuxI8BI8I8B {
         let fal = asm.fresh_variable(&params[2]);
         let en = asm.fresh_variable(&params[3]);
         let res = asm.fresh_variable(&instr.dst_id());
+        let wire_name = asm.new_variable_name();
+        let wire = verilog::Decl::new_wire(&wire_name, 8);
         for i in 0..8 {
-            let wire_name = asm.new_variable_name();
-            let wire = verilog::Decl::new_wire(&wire_name, 1);
             let mut lut = Lut::new_lut3();
             lut.set_id(&asm.new_instance_name());
             lut.set_init("ac");
             lut.add_input_with_index(&tru, i);
             lut.add_input_with_index(&fal, i);
             lut.add_input(&con);
-            lut.set_output(&wire_name);
+            lut.set_output_with_index(&wire_name, i);
             let mut reg = Reg::new_fdre();
             reg.set_id(&asm.new_instance_name());
             reg.set_clock(&asm.clock());
             reg.set_reset(&asm.reset());
             reg.set_en(&en);
-            reg.set_input(&wire_name);
+            reg.set_input_with_index(&wire_name, i);
             reg.set_output_with_index(&res, i);
             asm.add_lut(verilog::Stmt::from(lut));
             asm.add_reg(verilog::Stmt::from(reg));
-            asm.add_wire(verilog::Stmt::from(wire));
         }
+        asm.add_wire(verilog::Stmt::from(wire));
     }
 }
