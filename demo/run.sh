c547574c77bc46e6df7d8db961dde8f3568b7c47
Luis Vega <vegaluisjose@gmail.com>
2020-08-31 11:58:58 -0700

    add test for register program

--- demo/run.sh
+++ demo/run.sh
@@ -11,3 +11,4 @@ cargo run --bin demo
 cd demo
 
 docker run --rm --pid=host -v $PWD:$WORKSPACE_DIR -w $WORKSPACE_DIR "vivado" bash --login xsim.sh fsm
+docker run --rm --pid=host -v $PWD:$WORKSPACE_DIR -w $WORKSPACE_DIR "vivado" bash --login xsim.sh register
