98067f8038b74c73a9a12098a2acb31785908594
Luis Vega <vegaluisjose@gmail.com>
2020-08-31 08:12:44 -0700

    start working on dsp vector instr

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use reticle::frontend::parser::parse_from_file;
+use reticle::passes::map::{map_analysis, map_asm, map_loc};
+
 fn main() {
-    println!("main");
+    let prog = parse_from_file("examples/isa/vector/vadd_v4.ret");
+    let prog_with_loc = map_loc(prog.clone());
+    let analysis = map_analysis(prog_with_loc.clone());
+    let asm = map_asm(prog.clone());
+    println!("\n{}\n", prog);
+    println!("\n{}\n", prog_with_loc);
+    println!("\n{}\n", asm);
+    println!("\n{}\n", analysis);
 }
