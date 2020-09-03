44864333d77fb09fd53619c90ab8b8409b45903e
Luis Vega <vegaluisjose@gmail.com>
2020-09-03 12:47:31 -0700

    add vector support to const instr

--- src/backend/arch/ultrascale/isa/constant.rs
+++ src/backend/arch/ultrascale/isa/constant.rs
@@ -9,14 +9,25 @@ pub struct Constant;
 impl Emit for Constant {
     fn emit(asm: &mut Assembler, instr: asm::Instr) {
         let instr = instr.std().clone();
-        assert!(!instr.is_vector()); // temporary check for only scalars
-        let value = instr.indexed_attr(0).value();
         let width = instr.dst_ty().width();
-        let mut constant = Const::new(width, value);
-        let res = asm.fresh_scalar_variable(&instr.dst_id());
-        constant.set_id(&res);
-        constant.set_gnd(&asm.gnd());
-        constant.set_vcc(&asm.vcc());
-        asm.add_assignment(verilog::Stmt::from(constant));
+        if instr.is_vector() {
+            for i in 0..instr.dst_ty().length() {
+                let value = instr.indexed_attr(i as usize).value();
+                let mut constant = Const::new(width, value);
+                let res = asm.fresh_vector_variable(&instr.dst_id(), i);
+                constant.set_id(&res);
+                constant.set_gnd(&asm.gnd());
+                constant.set_vcc(&asm.vcc());
+                asm.add_assignment(verilog::Stmt::from(constant));
+            }
+        } else {
+            let value = instr.indexed_attr(0).value();
+            let mut constant = Const::new(width, value);
+            let res = asm.fresh_scalar_variable(&instr.dst_id());
+            constant.set_id(&res);
+            constant.set_gnd(&asm.gnd());
+            constant.set_vcc(&asm.vcc());
+            asm.add_assignment(verilog::Stmt::from(constant));
+        }
     }
 }
