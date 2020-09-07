3dc0af8eefc2e88a206f2cd9112944f0dd16167b
Luis Vega <vegaluisjose@gmail.com>
2020-09-07 15:21:00 -0700

    test another command

--- ci/ci_lint.sh
+++ ci/ci_lint.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 CARGO_CMD=$1
 
-$CARGO_CMD cargo --version
+docker run --rm --pid=host --user "$(id -u)":"$(id -g)" -v "$PWD":/usr/src/myapp -w /usr/src/myapp "reticle-rust" cargo --version
 
 # $CARGO_CMD cargo clippy --all-targets --all-features -- -D warnings
 # $CARGO_CMD cargo fmt -- --check
