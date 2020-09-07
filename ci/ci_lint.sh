157cc4bda0f9e15642e6d761a012860df268b108
Luis Vega <vegaluisjose@gmail.com>
2020-09-07 15:32:38 -0700

    test dummy cmd

--- ci/ci_lint.sh
+++ ci/ci_lint.sh
@@ -2,13 +2,17 @@
 
 set -eox pipefail
 
-DOCKER_RUST=$(docker run --rm \
---pid=host \
---user "$(id -u)":"$(id -g)" \
--v "$PWD":/usr/src/myapp \
--w /usr/src/myapp "reticle-rust")
+# DOCKER_RUST=$(docker run --rm \
+# --pid=host \
+# --user "$(id -u)":"$(id -g)" \
+# -v "$PWD":/usr/src/myapp \
+# -w /usr/src/myapp "reticle-rust")
 
-eval "$DOCKER_RUST" cargo --version
+# eval "$DOCKER_RUST" cargo --version
+
+CMD=$(date)
+
+eval "$CMD" | wc
 
 # $CARGO_CMD cargo clippy --all-targets --all-features -- -D warnings
 # $CARGO_CMD cargo fmt -- --check
