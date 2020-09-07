d981aece48600bf22d0dd41cab2263984e01fff6
Luis Vega <vegaluisjose@gmail.com>
2020-09-07 15:19:30 -0700

    check cargo version

--- ci/ci_lint.sh
+++ ci/ci_lint.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 CARGO_CMD=$1
 
-$CARGO_CMD ls
+$CARGO_CMD cargo --version
 
 # $CARGO_CMD cargo clippy --all-targets --all-features -- -D warnings
 # $CARGO_CMD cargo fmt -- --check
