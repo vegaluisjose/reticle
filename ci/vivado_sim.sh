f3c01e1256ed2eb3b65820badc6e98d695c1458b
Luis Vega <vegaluisjose@gmail.com>
2020-09-07 14:56:14 -0700

    update vivado commands

--- ci/vivado_sim.sh
+++ ci/vivado_sim.sh
@@ -9,5 +9,5 @@ source /tools/Xilinx/Vivado/2020.1/settings64.sh
 mkdir -p $OUT_DIR
 cd $OUT_DIR
 
-xvlog --sv --incr --relax $XILINX_VIVADO/data/verilog/src/glbl.v $WORKSPACE_DIR/${NAME}.v $WORKSPACE_DIR/test_${NAME}.v
+xvlog $XILINX_VIVADO/data/verilog/src/glbl.v $WORKSPACE_DIR/${NAME}.v $WORKSPACE_DIR/test_${NAME}.v
 xelab -L unisims_ver -R --timescale 1ps/1ps -O3 glbl test_${NAME}
