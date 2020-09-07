410b6c7cdcd4f672092c59abc3d257b66394d306
Luis Vega <vegaluisjose@gmail.com>
2020-09-07 15:17:19 -0700

    add breakpoint at different place

--- ci/ci_lint.sh
+++ ci/ci_lint.sh
@@ -4,5 +4,7 @@ set -eo pipefail
 
 CARGO_CMD=$1
 
-$CARGO_CMD cargo clippy --all-targets --all-features -- -D warnings
-$CARGO_CMD cargo fmt -- --check
+$CARGO_CMD ls
+
+# $CARGO_CMD cargo clippy --all-targets --all-features -- -D warnings
+# $CARGO_CMD cargo fmt -- --check
