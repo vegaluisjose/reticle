2538fe5fe1031e5385030dc04c15f3cbe5de6aab
Luis Vega <vegaluisjose@gmail.com>
2020-09-13 03:19:07 +0000

    emit input for dsp scalar

--- src/backend/arch/ultrascale/isa/dsp_scalar_arith.rs
+++ src/backend/arch/ultrascale/isa/dsp_scalar_arith.rs
@@ -6,7 +6,7 @@ use crate::backend::verilog;
 #[derive(Clone, Debug)]
 pub struct DspScalarArith;
 
-fn emit_scalar_op(instr: &asm::Instr) -> DspScalarOp {
+fn emit_op(instr: &asm::Instr) -> DspScalarOp {
     match instr.prim().op().as_ref() {
         "dsp_add_reg_mul_i8_i8_i8_b_i8" => DspScalarOp::MulAdd,
         _ => unimplemented!(),
@@ -20,14 +20,52 @@ fn emit_wire(asm: &mut Assembler, width: u64) -> String {
     name
 }
 
+fn emit_input(
+    asm: &mut Assembler,
+    instr: &asm::Instr,
+    wire: &str,
+    width: u64,
+    index: usize,
+    trunc: bool,
+) {
+    let mut concat = verilog::ExprConcat::default();
+    let w = instr.dst_ty().width();
+    let trunc_width = w / 2;
+    let rem = if trunc {
+        width - trunc_width
+    } else {
+        width - w
+    };
+    let name = asm.fresh_scalar_variable(&instr.indexed_param(index).id());
+    let hi = if trunc {
+        verilog::Expr::new_int((trunc_width - 1) as i32)
+    } else {
+        verilog::Expr::new_int((w - 1) as i32)
+    };
+    let lo = verilog::Expr::new_int(0);
+    concat.add_expr(verilog::Expr::new_slice(&name, hi, lo));
+    // extend sign
+    for _ in 0..rem {
+        concat.add_expr(verilog::Expr::new_index_bit(&name, (w - 1) as i32));
+    }
+    let src_expr = verilog::Expr::from(concat);
+    let dst_expr = verilog::Expr::new_ref(wire);
+    let assign = verilog::Parallel::ParAssign(dst_expr, src_expr);
+    asm.add_assignment(verilog::Stmt::from(assign));
+}
+
 impl Emit for DspScalarArith {
     fn emit(asm: &mut Assembler, instr: asm::Instr) {
-        let op = emit_scalar_op(&instr);
+        let op = emit_op(&instr);
         let mut dsp = DspScalar::new(op);
-        let a = emit_wire(asm, dsp.get_width("a"));
-        let b = emit_wire(asm, dsp.get_width("b"));
-        let c = emit_wire(asm, dsp.get_width("c"));
-        let y = emit_wire(asm, dsp.get_width("y"));
+        let aw = dsp.get_width("a");
+        let bw = dsp.get_width("b");
+        let cw = dsp.get_width("c");
+        let yw = dsp.get_width("y");
+        let a = emit_wire(asm, aw);
+        let b = emit_wire(asm, bw);
+        let c = emit_wire(asm, cw);
+        let y = emit_wire(asm, yw);
         let en_mul = asm.fresh_scalar_variable(&instr.indexed_param(2).id());
         dsp.set_id(&asm.new_instance_name());
         dsp.set_input("clock", &asm.clock());
@@ -37,6 +75,9 @@ impl Emit for DspScalarArith {
         dsp.set_input("c", &c);
         dsp.set_input("en_mul", &en_mul);
         dsp.set_output("y", &y);
+        emit_input(asm, &instr, &a, aw, 0, true);
+        emit_input(asm, &instr, &b, bw, 1, true);
+        emit_input(asm, &instr, &c, cw, 3, false);
         asm.add_instance(verilog::Stmt::from(dsp));
     }
 }
