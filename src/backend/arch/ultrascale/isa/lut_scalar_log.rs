33d9a1bd6b1f7b7a3dfe647f32e134a648eb6e03
Luis Vega <vegaluisjose@gmail.com>
2020-09-13 23:14:15 +0000

    merge two instruction expansion

--- src/backend/arch/ultrascale/isa/lut_scalar_log.rs
+++ src/backend/arch/ultrascale/isa/lut_scalar_log.rs
@@ -3,20 +3,28 @@ use crate::backend::arch::ultrascale::prim::ast::Lut;
 use crate::backend::asm::ast as asm;
 use crate::backend::verilog;
 
-#[derive(Clone, Debug)]
-pub struct LutOr;
+fn emit_lut_init(instr: &asm::Instr) -> String {
+    match instr.prim().op().as_ref() {
+        "lut_and_b_b_b" => "8".to_string(),
+        "lut_or_b_b_b" => "e".to_string(),
+        _ => unimplemented!(),
+    }
+}
 
-impl Emit for LutOr {
+#[derive(Clone, Debug)]
+pub struct LutScalarLog;
+
+impl Emit for LutScalarLog {
     fn emit(asm: &mut Assembler, instr: asm::Instr) {
-        let instr = instr.prim().clone();
         let params: Vec<String> = instr.params().iter().map(|x| x.id()).collect();
         let lhs = asm.fresh_scalar_variable(&params[0]);
         let rhs = asm.fresh_scalar_variable(&params[1]);
         let res = asm.fresh_scalar_variable(&instr.dst_id());
         let width = instr.dst_ty().width();
+        let init = emit_lut_init(&instr);
         for i in 0..width {
             let mut lut = Lut::new_lut2();
-            lut.set_init("e");
+            lut.set_init(&init);
             lut.set_id(&asm.new_instance_name());
             if width == 1 {
                 lut.add_input(&lhs);
--- src/backend/arch/ultrascale/isa/mod.rs
+++ src/backend/arch/ultrascale/isa/mod.rs
@@ -1,17 +1,15 @@
 mod constant;
 mod dsp_scalar_arith;
 mod dsp_vector_arith;
-mod lut_and;
 mod lut_eq;
 mod lut_mux;
-mod lut_or;
 mod lut_reg;
+mod lut_scalar_log;
 
 pub use constant::*;
 pub use dsp_scalar_arith::*;
 pub use dsp_vector_arith::*;
-pub use lut_and::*;
 pub use lut_eq::*;
 pub use lut_mux::*;
-pub use lut_or::*;
 pub use lut_reg::*;
+pub use lut_scalar_log::*;
