93cc58b918bd3c18a909e7f8da1d81e0b0252edb
Luis Vega <vegaluisjose@gmail.com>
2020-09-14 04:29:34 +0000

    move other lut instr to scalar

--- src/backend/arch/ultrascale/isa/mod.rs
+++ src/backend/arch/ultrascale/isa/mod.rs
@@ -1,13 +1,7 @@
 mod dsp_scalar_arith;
 mod dsp_vector_arith;
-mod lut_eq;
-mod lut_mux;
-mod lut_reg;
 pub mod scalar;
 pub mod vector;
 
 pub use dsp_scalar_arith::*;
 pub use dsp_vector_arith::*;
-pub use lut_eq::*;
-pub use lut_mux::*;
-pub use lut_reg::*;
--- src/backend/arch/ultrascale/isa/scalar/mod.rs
+++ src/backend/arch/ultrascale/isa/scalar/mod.rs
@@ -1,5 +1,11 @@
 mod constant;
+mod lut_eq;
 mod lut_logical;
+mod lut_mux;
+mod lut_reg;
 
 pub use constant::*;
+pub use lut_eq::*;
 pub use lut_logical::*;
+pub use lut_mux::*;
+pub use lut_reg::*;
