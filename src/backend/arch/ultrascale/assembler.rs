0ab797a876aa067807cba9cb229f4a9138f3e725
Luis Vega <vegaluisjose@gmail.com>
2020-09-15 02:20:40 +0000

    fix fmt

--- src/backend/arch/ultrascale/assembler.rs
+++ src/backend/arch/ultrascale/assembler.rs
@@ -278,7 +278,9 @@ impl Assembler {
                     "lut_not_b_b" => isa::scalar::LutNot::emit(self, instr),
                     "lut_reg_mux_i8_b_i8_i8_b" => isa::scalar::LutMux::emit(self, instr),
                     "lut_reg_i8_i8_b" => isa::scalar::LutReg::emit(self, instr),
-                    "dsp_add_reg_mul_i8_i8_i8_b_i8" => isa::scalar::DspFusedArith::emit(self, instr),
+                    "dsp_add_reg_mul_i8_i8_i8_b_i8" => {
+                        isa::scalar::DspFusedArith::emit(self, instr)
+                    }
                     "dsp_add_i8v4_i8v4_i8v4" => isa::vector::DspArith::emit(self, instr),
                     _ => unimplemented!(),
                 }
