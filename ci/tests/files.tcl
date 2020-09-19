1fb82c14824089cb36f483b437725c7ec05a9f18
Luis Vega <vegaluisjose@gmail.com>
2020-09-18 20:50:38 -0700

    mul_i8_i8_i8

--- ci/tests/files.tcl
+++ ci/tests/files.tcl
@@ -5,10 +5,12 @@ ${VERILOG_DIR}/add_i8v4_i8v4_i8v4.v
 ${VERILOG_DIR}/sub_i8v4_i8v4_i8v4.v
 ${VERILOG_DIR}/add_mul_i8_i8_i8_i8.v
 ${VERILOG_DIR}/add_reg_mul_i8_i8_i8_b_i8.v
+${VERILOG_DIR}/mul_i8_i8_i8.v
 ${TESTS_DIR}/test_add_i8_i8_i8.v
 ${TESTS_DIR}/test_sub_i8_i8_i8.v
 ${TESTS_DIR}/test_add_i8v4_i8v4_i8v4.v
 ${TESTS_DIR}/test_sub_i8v4_i8v4_i8v4.v
 ${TESTS_DIR}/test_add_mul_i8_i8_i8_i8.v
 ${TESTS_DIR}/test_add_reg_mul_i8_i8_i8_b_i8.v
+${TESTS_DIR}/test_mul_i8_i8_i8.v
 ${TESTS_DIR}/test_all.v
--- ci/tests/test_all.v
+++ ci/tests/test_all.v
@@ -15,8 +15,8 @@ module test_all();
 
     assign reset = ~start | glbl.GSR;
 
-    wire [5:0] fail;
-    wire [5:0] finish;
+    wire [6:0] fail;
+    wire [6:0] finish;
 
     test_add_i8_i8_i8 t0(.clock(clock), .reset(reset), .fail(fail[0]), .finish(finish[0]));
     test_sub_i8_i8_i8 t1(.clock(clock), .reset(reset), .fail(fail[1]), .finish(finish[1]));
@@ -24,6 +24,7 @@ module test_all();
     test_sub_i8v4_i8v4_i8v4 t3(.clock(clock), .reset(reset), .fail(fail[3]), .finish(finish[3]));
     test_add_mul_i8_i8_i8_i8 t4(.clock(clock), .reset(reset), .fail(fail[4]), .finish(finish[4]));
     test_add_reg_mul_i8_i8_i8_b_i8 t5(.clock(clock), .reset(reset), .fail(fail[5]), .finish(finish[5]));
+    test_mul_i8_i8_i8 t6(.clock(clock), .reset(reset), .fail(fail[6]), .finish(finish[6]));
 
     always @(posedge clock) begin
         if (|fail) begin
