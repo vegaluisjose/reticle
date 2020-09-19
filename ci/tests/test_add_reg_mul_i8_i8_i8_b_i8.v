fcc3559769044915205bbf68417b713bf0e7528e
Luis Vega <vegaluisjose@gmail.com>
2020-09-18 20:44:21 -0700

    test_add_reg_mul_i8_i8_i8_b_i8

--- ci/tests/test_add_reg_mul_i8_i8_i8_b_i8.v
+++ ci/tests/test_add_reg_mul_i8_i8_i8_b_i8.v
@@ -1,17 +1,12 @@
-module test_add_reg_mul_i8_i8_i8_b_i8();
-
-    reg clock = 1'b0;
-    reg reset = 1'b0;
-
-    always #500 clock = ~clock;
-
-    initial begin
-        reset = 1'b1;
-        repeat(16)@(negedge clock);
-        reset = 1'b0;
-    end
+module test_add_reg_mul_i8_i8_i8_b_i8(
+    input clock,
+    input reset,
+    output fail,
+    output finish);
 
     reg [31:0] step;
+    reg t_fail;
+    reg t_finish;
 
     reg [7:0] a;
     reg [7:0] b;
@@ -26,6 +21,8 @@ module test_add_reg_mul_i8_i8_i8_b_i8();
             b <= 8'd2;
             c <= 8'd3;
             en <= 1'b1;
+            t_fail <= 1'b0;
+            t_finish <= 1'b0;
         end
         else begin
             case (step)
@@ -36,21 +33,24 @@ module test_add_reg_mul_i8_i8_i8_b_i8();
                     c <= 8'd3;
                     en <= 1'b0;
                     if (y != 8'd3) begin
-                        $display("~~FAIL~~");
-                        $finish;
+                        $display("[test_add_reg_mul_i8_i8_i8_b_i8] ~~FAIL~~ res:%d exp:3", $signed(y));
+                        t_fail <= 1'b1;
                     end
                 end
                 1: begin
                     if (y != 8'd11) begin
-                        $display("~~FAIL~~");
-                        $finish;
+                        $display("[test_add_reg_mul_i8_i8_i8_b_i8] ~~FAIL~~ res:%d exp:11", $signed(y));
+                        t_fail <= 1'b1;
                     end
-                    $finish;
+                    t_finish <= 1'b1;
                 end
             endcase
         end
     end
 
     add_reg_mul_i8_i8_i8_b_i8 dut(.clock(clock), .reset(reset), .a(a), .b(b), .c(c), .en(en), .y(y));
+
+    assign fail = t_fail;
+    assign finish = t_finish;
 
 endmodule
--- ci/tests/test_all.v
+++ ci/tests/test_all.v
@@ -15,14 +15,15 @@ module test_all();
 
     assign reset = ~start | glbl.GSR;
 
-    wire [4:0] fail;
-    wire [4:0] finish;
+    wire [5:0] fail;
+    wire [5:0] finish;
 
     test_add_i8_i8_i8 t0(.clock(clock), .reset(reset), .fail(fail[0]), .finish(finish[0]));
     test_sub_i8_i8_i8 t1(.clock(clock), .reset(reset), .fail(fail[1]), .finish(finish[1]));
     test_add_i8v4_i8v4_i8v4 t2(.clock(clock), .reset(reset), .fail(fail[2]), .finish(finish[2]));
     test_sub_i8v4_i8v4_i8v4 t3(.clock(clock), .reset(reset), .fail(fail[3]), .finish(finish[3]));
     test_add_mul_i8_i8_i8_i8 t4(.clock(clock), .reset(reset), .fail(fail[4]), .finish(finish[4]));
+    test_add_reg_mul_i8_i8_i8_b_i8 t5(.clock(clock), .reset(reset), .fail(fail[5]), .finish(finish[5]));
 
     always @(posedge clock) begin
         if (|fail) begin
