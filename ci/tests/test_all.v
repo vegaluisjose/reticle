4ca3eda4c120c6155337c8af36780a47f2094db5
Luis Vega <vegaluisjose@gmail.com>
2020-09-18 20:27:06 -0700

    update test all

--- ci/tests/test_all.v
+++ ci/tests/test_all.v
@@ -15,12 +15,13 @@ module test_all();
 
     assign reset = ~start | glbl.GSR;
 
-    wire [2:0] fail;
-    wire [2:0] finish;
+    wire [3:0] fail;
+    wire [3:0] finish;
 
     test_add_i8_i8_i8 t0(.clock(clock), .reset(reset), .fail(fail[0]), .finish(finish[0]));
     test_sub_i8_i8_i8 t1(.clock(clock), .reset(reset), .fail(fail[1]), .finish(finish[1]));
-    test_sub_i8v4_i8v4_i8v4 t2(.clock(clock), .reset(reset), .fail(fail[2]), .finish(finish[2]));
+    test_add_i8v4_i8v4_i8v4 t2(.clock(clock), .reset(reset), .fail(fail[2]), .finish(finish[2]));
+    test_sub_i8v4_i8v4_i8v4 t3(.clock(clock), .reset(reset), .fail(fail[3]), .finish(finish[3]));
 
     always @(posedge clock) begin
         if (|fail) begin
