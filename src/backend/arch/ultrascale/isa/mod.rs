d55086ce1f9fd1457ebace633374f40d22742855
Luis Vega <vegaluisjose@gmail.com>
2020-09-26 17:24:35 -0700

    remove file

--- src/backend/arch/ultrascale/isa/mod.rs
+++ src/backend/arch/ultrascale/isa/mod.rs
@@ -5,7 +5,6 @@ mod lut_add_sub;
 mod lut_eq;
 mod lut_logical;
 mod lut_mux;
-mod lut_not;
 mod lut_reg;
 
 pub use constant::*;
@@ -15,5 +14,4 @@ pub use lut_add_sub::*;
 pub use lut_eq::*;
 pub use lut_logical::*;
 pub use lut_mux::*;
-pub use lut_not::*;
 pub use lut_reg::*;
