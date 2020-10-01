4838505aa5b103b8655b320f1afaeb394734482a
Luis Vega <vegaluisjose@gmail.com>
2020-10-01 21:41:04 +0000

    refactor vector add func

--- examples/paper/vector-add/run.py
+++ examples/paper/vector-add/run.py
@@ -15,29 +15,34 @@ def make_dir(value):
         makedirs(value)
 
 
-if __name__ == "__main__":
-    lengths = [8, 16]
+def run_vector_add(name, dirname, lengths):
     backends = ["gen", "dsp", "ret"]
-    dirname = "results"
     result_dir = path.join(get_curr_dir(), dirname)
     vivado_dir = path.join(docker_vivado_workdir(), dirname)
     make_dir(result_dir)
     build_reticle()
     for l in lengths:
-        name = "vadd{}".format(l)
-        prog = path.join(result_dir, "{}.ret".format(name))
-        vector_add(name, l, prog)
+        pname = "{}{}".format(name, l)
+        prog = path.join(result_dir, "{}.ret".format(pname))
+        vector_add(pname, l, prog)
         for b in backends:
             use_dsp = True if b == "dsp" else False
-            vname = "{}_{}".format(name, b)
+            vname = "{}_{}".format(pname, b)
             vfile = path.join(result_dir, "{}.v".format(vname))
             if b == "ret":
                 compile_reticle(prog, vfile)
                 run_vivado(
-                    ["vivado.sh", "generic.tcl", vname, vivado_dir, name]
+                    ["vivado.sh", "generic.tcl", vname, vivado_dir, pname]
                 )
             else:
                 compile_generic(prog, vfile, use_dsp)
                 run_vivado(
-                    ["vivado.sh", "generic.tcl", vname, vivado_dir, name]
+                    ["vivado.sh", "generic.tcl", vname, vivado_dir, pname]
                 )
+
+
+if __name__ == "__main__":
+    lengths = [8, 16]
+    dirname = "results"
+    name = "vadd"
+    run_vector_add(name, dirname, lengths)
