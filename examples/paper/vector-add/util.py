ea3cf3f1e2db1c07183690e6d3159df318420c5e
Luis Vega <vegaluisjose@gmail.com>
2020-10-08 14:40:05 -0700

    remove these files

--- examples/paper/vector-add/util.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def get_curr_dir():
-    return os.path.dirname(os.path.abspath(os.path.expanduser(__file__)))
-
-
-def get_rust_dir():
-    return os.path.abspath(os.path.join(get_curr_dir(), "..", "..", ".."))
--- examples/paper/vector-add/vivado.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VIVADO_TCL=$1
-FILENAME=$2
-OUT_DIR=$3
-NAME=$4
-
-source /tools/Xilinx/Vivado/2020.1/settings64.sh
-vivado -mode batch -source $VIVADO_TCL -tclargs $FILENAME $OUT_DIR $NAME
