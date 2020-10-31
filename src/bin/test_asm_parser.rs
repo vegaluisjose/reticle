f65e023c4fd65d1b787b4cba00efb96079cddb7c
Luis Vega <vegaluisjose@gmail.com>
2020-10-31 16:08:41 -0700

    add new asm prog

--- src/bin/test_asm_parser.rs
+++ src/bin/test_asm_parser.rs
@@ -1,6 +1,6 @@
 use reticle::asm::parser::parse_from_file;
 
 fn main() {
-    let prog = parse_from_file("examples/asm/example.rasm");
+    let prog = parse_from_file("examples/asm/dot.rasm");
     println!("{}", prog);
 }
