4d74c608926be80a5a2e4908ebd5f1e65e188cd1
Luis Vega <vegaluisjose@gmail.com>
2020-11-13 21:54:30 -0800

    add placer object

--- src/passes/layout/place.rs
+++ src/passes/layout/place.rs
@@ -1,9 +1,79 @@
-use crate::asm::ast::Prog;
+use crate::asm::ast::{InstrPhy, Prim, Prog};
+use std::collections::HashMap;
+
+#[derive(Clone, Debug)]
+pub struct PlacerInput {
+    pub id: u32,
+    pub prim: Prim,
+}
+
+impl PlacerInput {
+    pub fn new(id: u32, prim: Prim) -> PlacerInput {
+        PlacerInput { id, prim }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Placer {
+    pub counter: u32,
+    pub var: HashMap<String, u32>,
+    pub num: HashMap<u32, String>,
+    pub inputs: Vec<PlacerInput>,
+}
+
+impl Default for Placer {
+    fn default() -> Self {
+        Placer {
+            counter: 0,
+            var: HashMap::new(),
+            num: HashMap::new(),
+            inputs: Vec::new(),
+        }
+    }
+}
+
+impl Placer {
+    pub fn counter(&self) -> u32 {
+        self.counter
+    }
+
+    pub fn inputs(&self) -> &Vec<PlacerInput> {
+        &self.inputs
+    }
+
+    pub fn rename(&mut self, current: &str) -> u32 {
+        let value = self.counter();
+        self.counter += 1;
+        self.var.insert(current.to_string(), value);
+        self.num.insert(value, current.to_string());
+        value
+    }
+
+    pub fn add_input(&mut self, id: u32, prim: Prim) {
+        let input = PlacerInput::new(id, prim);
+        self.inputs.push(input);
+    }
+
+    pub fn add_instr(&mut self, instr: &InstrPhy) {
+        let id = self.rename(&instr.dst().id());
+        self.add_input(id, instr.loc().prim().clone());
+    }
+
+    pub fn run(&mut self) {
+        for i in self.inputs() {
+            println!("{:?}", i);
+        }
+    }
+}
 
 pub fn place_basic(prog: &Prog) {
+    let mut placer = Placer::default();
     for instr in prog.body() {
-        println!("{}", instr.dst().id());
+        if instr.is_phy() {
+            placer.add_instr(instr.phy());
+        }
     }
+    placer.run();
 }
 
 // use reticle::util::file::{create_absolute, write_to_tempfile};
