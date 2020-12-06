6e1c000359aa0adce73550ea606dd85ad7b899c1
Luis Vega <vegaluisjose@gmail.com>
2020-12-05 19:01:57 -0800

    start working with asm

---
+++ src/v2/asm/ast.rs
@@ -0,0 +1,50 @@
+use crate::v2::il::ast as il;
+
+pub type Id = il::Id;
+pub type Ty = il::Ty;
+pub type Prim = il::Prim;
+pub type ExprTup = il::ExprTup;
+pub type Expr = il::Expr;
+pub type WireOp = il::WireOp;
+pub type InstrWire = il::InstrWire;
+pub type Sig = il::Sig;
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum ExprCoord {
+    Var,
+    Name(Id),
+    Lit(u32),
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct Loc {
+    pub prim: Prim,
+    pub x: ExprCoord,
+    pub y: ExprCoord,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum AsmOp {
+    Op(Id),
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct InstrAsm {
+    pub op: AsmOp,
+    pub dst: Expr,
+    pub attr: Expr,
+    pub arg: Expr,
+    pub loc: Loc,
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub enum Instr {
+    Wire(InstrWire),
+    Asm(InstrAsm),
+}
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+pub struct Prog {
+    pub sig: Sig,
+    pub body: Vec<Instr>,
+}
