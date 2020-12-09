d95e7eb4e42d6ba49a5afa7c0613ee6e1a48c0cd
Luis Vega <vegaluisjose@gmail.com>
2020-12-09 12:34:19 -0800

    update

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -3,12 +3,18 @@ use reticle::ir::parser::IRParser;
 use reticle::tdl::parser::TDLParser;
 
 fn main() {
-    let ir = IRParser::parse_from_str("main -> (y:i8) { y:i8 = const[3]; }");
-    let asm = AsmParser::parse_from_str("main -> (y:i8) { y:i8 = lut_reg[3](a, b) @lut; }");
-    let tdl = TDLParser::parse_from_str(
-        "main[lut, 0, 1](a:i8, b:bool) -> (y:i8) { y:i8 = reg[3](a, b); }",
+    let ir =
+        IRParser::parse_from_str("main(a:bool, b:bool) -> (y:bool) { y:bool = reg[0](a, b); }");
+    let asm = AsmParser::parse_from_str(
+        "main(a:bool, b:bool) -> (y:bool) { y:bool = lut_reg(a, b) @lut; }",
     );
+    let tdl = TDLParser::parse_from_str(
+        "lut_reg[lut, 0, 1](a:bool, b:bool) -> (y:bool) { y:bool = reg[0](a, b); }",
+    );
+    let pdl =
+        String::from("lut_reg(a:bool, b:bool) -> (y:bool) { y:bool = fdre(a, b) @bel(x, y); }");
     println!("{}", ir.unwrap());
     println!("{}", asm.unwrap());
     println!("{}", tdl.unwrap());
+    println!("{}", pdl);
 }
