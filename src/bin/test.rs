6bcbacbcf363d5e23e18e9538b0044b3e0624c55
Luis Vega <vegaluisjose@gmail.com>
2020-12-14 23:35:32 -0800

    update

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -12,7 +12,7 @@ fn main() {
     let tdl = TDLParser::parse_from_str(
         "lut_reg[lut, 0, 1](a:bool, b:bool) -> (y:bool) { y:bool = reg[0](a, b); }",
     );
-    let mach = MLParser::parse_from_str("a = sll;");
+    let mach = MLParser::parse_from_str("a:i8 = sll;");
     println!("{}", ir.unwrap());
     println!("{}", asm.unwrap());
     println!("{}", tdl.unwrap());
