227b06654cc7304f327ec09cffb68f918141cb94
Luis Vega <vegaluisjose@gmail.com>
2020-12-16 12:22:48 -0800

    update syntax

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -7,10 +7,10 @@ fn main() {
     let ir =
         IRParser::parse_from_str("def main(a:bool, b:bool) -> (y:bool) { y:bool = reg[0](a, b); }");
     let asm = AsmParser::parse_from_str(
-        "main(a:bool, b:bool) -> (y:bool) { y:bool = lut_reg(a, b) @lut(x+1,y); }",
+        "def main(a:bool, b:bool) -> (y:bool) { y:bool = lut_reg(a, b) @lut(x+1,y); }",
     );
     let tdl = TDLParser::parse_from_str(
-        "lut_reg[lut, 0, 1](a:bool, b:bool) -> (y:bool) { y:bool = reg[0](a, b); }",
+        "def lut_reg[lut, 0, 1](a:bool, b:bool) -> (y:bool) { y:bool = reg[0](a, b); }",
     );
     let mach = MLParser::parse_from_str("def main() -> () { y:i8 = dsp[ra=1](a,b) @(1,2); }");
     println!("{}", ir.unwrap());
