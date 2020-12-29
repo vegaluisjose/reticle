05b83740d314837197d3d6295f32a86445d0e150
Luis Vega <vegaluisjose@gmail.com>
2020-12-29 09:12:21 -0800

    impl from for expr to expr conversion

--- src/verilog/from.rs
+++ src/verilog/from.rs
@@ -32,6 +32,34 @@ impl From<ir::ExprTup> for Vec<verilog::Id> {
     }
 }
 
+impl From<ir::ExprTerm> for Vec<verilog::Expr> {
+    fn from(term: ir::ExprTerm) -> Self {
+        let mut exprs: Vec<verilog::Expr> = Vec::new();
+        match term {
+            ir::ExprTerm::Any => exprs.push(verilog::Expr::new_ref("")),
+            ir::ExprTerm::Var(_, _) => {
+                let ids: Vec<verilog::Id> = term.clone().into();
+                for id in ids {
+                    exprs.push(verilog::Expr::new_ref(&id));
+                }
+            }
+            _ => (),
+        }
+        exprs
+    }
+}
+
+impl From<ir::ExprTup> for Vec<verilog::Expr> {
+    fn from(tup: ir::ExprTup) -> Self {
+        let mut exprs: Vec<verilog::Expr> = Vec::new();
+        for t in tup.term() {
+            let v: Vec<verilog::Expr> = t.clone().into();
+            exprs.extend(v)
+        }
+        exprs
+    }
+}
+
 impl From<ir::ExprTerm> for Vec<verilog::Decl> {
     fn from(term: ir::ExprTerm) -> Self {
         let ids: Vec<verilog::Id> = term.clone().into();
@@ -60,6 +88,15 @@ impl From<ir::ExprTup> for Vec<verilog::Decl> {
 }
 
 impl From<ir::Expr> for Vec<verilog::Id> {
+    fn from(expr: ir::Expr) -> Self {
+        match &expr {
+            ir::Expr::Tup(tup) => tup.clone().into(),
+            ir::Expr::Term(term) => term.clone().into(),
+        }
+    }
+}
+
+impl From<ir::Expr> for Vec<verilog::Expr> {
     fn from(expr: ir::Expr) -> Self {
         match &expr {
             ir::Expr::Tup(tup) => tup.clone().into(),
