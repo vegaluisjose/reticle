dc6bb8e3769518dfea606d0ec5a36e4c068846f1
Luis Vega <vegaluisjose@gmail.com>
2021-01-04 10:44:23 -0800

    add from expr to vec of exprterm

--- src/ir/from.rs
+++ src/ir/from.rs
@@ -31,6 +31,15 @@ impl From<InstrComp> for Instr {
     }
 }
 
+impl From<Expr> for Vec<ExprTerm> {
+    fn from(expr: Expr) -> Self {
+        match expr {
+            Expr::Tup(tup) => tup.term().clone(),
+            Expr::Term(term) => vec![term.clone()],
+        }
+    }
+}
+
 impl From<Expr> for HashSet<Expr> {
     fn from(expr: Expr) -> Self {
         let mut set: HashSet<Expr> = HashSet::new();
