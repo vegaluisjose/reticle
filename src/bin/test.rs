6c4bdc3615ac738d44cad5387af8e07bfba43ea9
Luis Vega <vegaluisjose@gmail.com>
2021-01-04 09:43:46 -0800

    add file

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -3,7 +3,7 @@ use reticle::xml::parser::MLParser;
 use std::convert::TryFrom;
 
 fn main() {
-    let parsed_ml = MLParser::parse_from_file("examples/vadd.rml");
+    let parsed_ml = MLParser::parse_from_file("examples/vadd.xml");
     if let Ok(prog) = parsed_ml {
         println!("{}", &prog);
         println!("{}", verilog::Module::try_from(prog).unwrap());
