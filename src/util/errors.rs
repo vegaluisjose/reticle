871bdf1247626d8a8a8553981d02b408c27756cb
Luis Vega <vegaluisjose@gmail.com>
2021-01-04 14:49:27 -0800

    add error

--- src/util/errors.rs
+++ src/util/errors.rs
@@ -3,23 +3,28 @@ use std::fmt;
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Error::ParseError(msg) => write!(f, "[Error][Parsing] {}", msg),
-            Error::ConvError(msg) => write!(f, "[Error][Conversion] {}", msg),
+            Error::Parse(msg) => write!(f, "[Error][Parsing] {}", msg),
+            Error::Conversion(msg) => write!(f, "[Error][Conversion] {}", msg),
+            Error::Type(msg) => write!(f, "[Error][Type] {}", msg),
         }
     }
 }
 
 #[derive(Debug)]
 pub enum Error {
-    ParseError(String),
-    ConvError(String),
+    Parse(String),
+    Conversion(String),
+    Type(String),
 }
 
 impl Error {
     pub fn new_parse_error(msg: &str) -> Self {
-        Error::ParseError(msg.to_string())
+        Error::Parse(msg.to_string())
     }
     pub fn new_conv_error(msg: &str) -> Self {
-        Error::ConvError(msg.to_string())
+        Error::Conversion(msg.to_string())
+    }
+    pub fn new_type_error(msg: &str) -> Self {
+        Error::Conversion(msg.to_string())
     }
 }
