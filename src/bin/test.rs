d059c0daf36fdf6c3b5b3a5ad0d87b7f77b85197
Luis Vega <vegaluisjose@gmail.com>
2021-01-07 11:08:57 -0800

    use other file

--- src/bin/test.rs
+++ src/bin/test.rs
@@ -3,7 +3,7 @@ use reticle::xl::parser::XLParser;
 use std::convert::TryFrom;
 
 fn main() {
-    let parsed_ml = XLParser::parse_from_file("examples/vadd.xl");
+    let parsed_ml = XLParser::parse_from_file("examples/tadd.xl");
     if let Ok(prog) = parsed_ml {
         println!("{}", &prog);
         println!("{}", verilog::Module::try_from(prog).unwrap());
