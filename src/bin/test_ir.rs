745bfb701671c8e8381bdb2c5438188071f3ed70
Luis Vega <vegaluisjose@gmail.com>
2021-01-16 09:58:25 -0800

    rename

--- src/bin/test_ir.rs
+++ src/bin/test_ir.rs
@@ -3,15 +3,11 @@ use reticle::verilog::ast as verilog;
 use std::convert::TryFrom;
 
 fn main() {
-    let prog = IRParser::parse_from_file("examples/add.ir");
+    let prog = IRParser::parse_from_file("examples/add_reg.ir");
     if let Ok(p) = prog {
         println!("{}", &p);
-        println!("{}", verilog::Module::try_from(p).unwrap());
+        if let Ok(v) = verilog::Module::try_from(p) {
+            println!("{}", v);
+        }
     }
-    // if let Ok(prog) = parsed_ml {
-    //     println!("{}", &prog);
-    //     println!("{}", verilog::Module::try_from(prog).unwrap());
-    // } else {
-    //     println!("{}", parsed_ml.unwrap());
-    // }
 }
