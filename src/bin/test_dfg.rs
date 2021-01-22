cb15a3d69f3ea216fd1f90a5b31a30074f264c79
Luis Vega <vegaluisjose@gmail.com>
2021-01-21 17:53:47 -0800

    save progress

--- src/bin/test_dfg.rs
+++ src/bin/test_dfg.rs
@@ -1,12 +1,48 @@
-use reticle::ir::dfg::Dfg;
+// use reticle::ir::dfg::Dfg;
+use reticle::ir::ast as ir;
 use reticle::ir::parser::IRParser;
-use std::convert::TryFrom;
+// use std::convert::TryFrom;
+// use petgraph::visit::Dfs;
+use std::collections::{HashMap, HashSet};
+
+fn find_roots(def: &ir::Def) {
+    let mut count: HashMap<ir::Id, u64> = HashMap::new();
+    let mut root: HashSet<ir::Id> = HashSet::new();
+    for instr in def.body() {
+        if instr.is_comp() {
+            let dst: Vec<ir::ExprTerm> = instr.dst().clone().into();
+            for e in dst {
+                if let Some(id) = e.id() {
+                    count.insert(id, 0);
+                }
+            }
+        }
+    }
+    for instr in def.body() {
+        let arg: Vec<ir::ExprTerm> = instr.arg().clone().into();
+        for e in arg {
+            if let Some(id) = e.id() {
+                if let Some(val) = count.get_mut(&id) {
+                    *val += 1;
+                }
+            }
+        }
+    }
+    for (k, v) in count {
+        if v != 1 {
+            root.insert(k);
+        }
+    }
+    for r in root {
+        println!("root --> {}", r);
+    }
+}
 
 fn main() {
     let prog = IRParser::parse_from_file("examples/fsm.ir");
     if let Ok(p) = prog {
-        println!("Program:\n{}", &p);
-        let dfg = Dfg::try_from(p).unwrap();
-        println!("Graphviz:\n{}", dfg);
+        if let Some(d) = p.get("main") {
+            find_roots(d);
+        }
     }
 }
