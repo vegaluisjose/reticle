2b6bbca475a576fc2b11175c39a56985ab00b274
Luis Vega <vegaluisjose@gmail.com>
2021-01-23 09:38:47 -0800

    refactor find tree root func

--- src/bin/test_dfg.rs
+++ src/bin/test_dfg.rs
@@ -5,9 +5,10 @@ use reticle::ir::parser::IRParser;
 // use petgraph::visit::Dfs;
 use std::collections::{HashMap, HashSet};
 
-fn find_roots(def: &ir::Def) {
+fn find_tree_root(def: &ir::Def) -> HashSet<ir::Id> {
     let mut count: HashMap<ir::Id, u64> = HashMap::new();
     let mut root: HashSet<ir::Id> = HashSet::new();
+    // store compute instructions
     for instr in def.body() {
         if instr.is_comp() {
             let dst: Vec<ir::ExprTerm> = instr.dst().clone().into();
@@ -18,6 +19,7 @@ fn find_roots(def: &ir::Def) {
             }
         }
     }
+    // calculate the number of times compute instructions are used
     for instr in def.body() {
         let arg: Vec<ir::ExprTerm> = instr.arg().clone().into();
         for e in arg {
@@ -28,21 +30,34 @@ fn find_roots(def: &ir::Def) {
             }
         }
     }
+    // a node is considered a root if:
+    // a node is an output (v=0)
+    // a node is used more than one time (v>1)
     for (k, v) in count {
         if v != 1 {
             root.insert(k);
         }
     }
-    for r in root {
-        println!("root --> {}", r);
+    // add the outputs that are not compute instructions as roots
+    let output: Vec<ir::ExprTerm> = def.output().clone().into();
+    for e in output {
+        if let Some(id) = e.id() {
+            if !root.contains(&id) {
+                root.insert(id);
+            }
+        }
     }
+    root
 }
 
 fn main() {
     let prog = IRParser::parse_from_file("examples/fsm.ir");
     if let Ok(p) = prog {
         if let Some(d) = p.get("main") {
-            find_roots(d);
+            let root = find_tree_root(d);
+            for r in root {
+                println!("root --> {}", r);
+            }
         }
     }
 }
