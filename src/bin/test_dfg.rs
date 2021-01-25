e9b8c683c3e91d4c57b45ad8c7b686802b03897a
Luis Vega <vegaluisjose@gmail.com>
2021-01-24 16:35:44 -0800

    save progress

--- src/bin/test_dfg.rs
+++ src/bin/test_dfg.rs
@@ -1,19 +1,18 @@
-// use reticle::ir::dfg::Dfg;
+use reticle::ir::dfg::Dfg;
 use reticle::ir::ast as ir;
 use reticle::ir::parser::IRParser;
-// use std::convert::TryFrom;
+use std::convert::TryFrom;
 // use petgraph::visit::Dfs;
+use std::convert::TryInto;
 use std::collections::{HashMap, HashSet};
 
-fn find_tree_root(def: &ir::Def) -> HashSet<ir::Id> {
+fn find_tree_root(def: &ir::Def) -> Vec<ir::Id> {
     let mut count: HashMap<ir::Id, u64> = HashMap::new();
-    let mut root: HashSet<ir::Id> = HashSet::new();
     // store compute instructions
     for instr in def.body() {
         if instr.is_comp() {
-            let dst: Vec<ir::ExprTerm> = instr.dst().clone().into();
-            for e in dst {
-                if let Some(id) = e.id() {
+            if let Some(term) = instr.dst().term() {
+                if let Some(id) = term.id() {
                     count.insert(id, 0);
                 }
             }
@@ -30,34 +29,79 @@ fn find_tree_root(def: &ir::Def) -> HashSet<ir::Id> {
             }
         }
     }
-    // a node is considered a root if:
-    // a node is an output (v=0)
-    // a node is used more than one time (v>1)
+    let mut root: Vec<ir::Id> = Vec::new();
+    // a node is a root if it is used more than once
     for (k, v) in count {
-        if v != 1 {
-            root.insert(k);
+        if v > 1 {
+            root.push(k);
         }
     }
-    // add the outputs that are not compute instructions as roots
+    // add outputs as roots
     let output: Vec<ir::ExprTerm> = def.output().clone().into();
     for e in output {
         if let Some(id) = e.id() {
-            if !root.contains(&id) {
-                root.insert(id);
-            }
+            root.push(id);
         }
     }
     root
+}
+
+fn walk(map: &HashMap<ir::Id, ir::Instr>, visited: &HashSet<ir::Id>, node: &ir::Id) -> HashSet<ir::Id> {
+    let mut visited = visited.clone();
+    let mut stack: Vec<ir::Id> = Vec::new();
+    stack.push(node.clone());
+    visited.insert(node.clone());
+    while let Some(cur) = stack.pop() {
+        println!("visiting {}", &cur);
+        if let Some(instr) = map.get(&cur) {
+            let args: Vec<ir::Id> = instr.arg().clone().try_into().unwrap();
+            for a in args {
+                if !visited.contains(&a) {
+                    visited.insert(a.clone());
+                    stack.push(a);
+                }
+            }
+        }
+    }
+    visited
+}
+
+fn create_tree_from_def(def: &ir::Def) {
+    let mut map: HashMap<ir::Id, ir::Instr> = HashMap::new();
+    // create instruction map
+    for instr in def.body() {
+        if let Some(term) = instr.dst().term() {
+            if let Some(id) = term.id() {
+                map.insert(id, instr.clone());
+            }
+        }
+    }
+    let root = find_tree_root(def);
+    // add inputs as visited
+    let mut visited: HashSet<ir::Id> = HashSet::new();
+    let input: Vec<ir::ExprTerm> = def.input().clone().into();
+    for e in input {
+        if let Some(id) = e.id() {
+            visited.insert(id.clone());
+        }
+    }
+    // walk
+    for r in root {
+        println!("debug {}", &r);
+        visited = walk(&map, &visited, &r);
+    }
 }
 
 fn main() {
     let prog = IRParser::parse_from_file("examples/fsm.ir");
     if let Ok(p) = prog {
         if let Some(d) = p.get("main") {
-            let root = find_tree_root(d);
-            for r in root {
-                println!("root --> {}", r);
-            }
+            println!("{}", Dfg::try_from(d.clone()).unwrap());
+            create_tree_from_def(d);
+            // let root = find_tree_root(d);
+            // for r in root {
+            //     println!("root --> {}", r);
+            // }
         }
     }
 }
