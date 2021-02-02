2a92ab5c498131a4b97118cce98d1f6f549df081
Luis Vega <vegaluisjose@gmail.com>
2021-02-02 13:08:22 -0800

    add matching func

--- src/bin/test_codegen.rs
+++ src/bin/test_codegen.rs
@@ -1,17 +1,18 @@
+use itertools::izip;
 use reticle::ir::parser::IRParser;
-use reticle::tdl::parser::TDLParser;
 use reticle::ir::partition::{Forest, Tree};
+use reticle::tdl::parser::TDLParser;
 use reticle::util::errors::Error;
 use std::convert::TryFrom;
 
-// fn tree_from_pats(file: &str) -> Result<Vec<(String, Tree)>, Error> {
-//     let mut pattern: Vec<(String, Tree)> = Vec::new();
-//     let target = TDLParser::parse_from_file(file)?;
-//     for (n, p) in target.pat() {
-//         pattern.push((n.clone(), Tree::try_from(p.clone())?));
-//     }
-//     Ok(pattern)
-// }
+fn tree_from_pats(file: &str) -> Result<Vec<(String, Tree)>, Error> {
+    let mut pattern: Vec<(String, Tree)> = Vec::new();
+    let target = TDLParser::parse_from_file(file)?;
+    for (n, p) in target.pat() {
+        pattern.push((n.clone(), Tree::try_from(p.clone())?));
+    }
+    Ok(pattern)
+}
 
 fn tree_from_prog(file: &str) -> Result<Vec<Tree>, Error> {
     let prog = IRParser::parse_from_file(file)?;
@@ -23,35 +24,34 @@ fn tree_from_prog(file: &str) -> Result<Vec<Tree>, Error> {
     }
 }
 
+fn is_match(block: &Tree, pat: &Tree, start: u64) -> bool {
+    if let Some(node) = block.node(start) {
+        println!("index: {} node: {}", start, node);
+    }
+    let bindex = block.bfs(start);
+    let pindex = pat.bfs(0);
+    let mut is_match = true;
+    for (p, b) in izip!(&pindex, &bindex) {
+        if let Some(pnode) = pat.node(*p) {
+            if let Some(bnode) = block.node(*b) {
+                if pnode.ty() != bnode.ty() {
+                    is_match = false;
+                }
+            }
+        }
+    }
+    is_match
+}
 
 fn main() -> Result<(), Error> {
-    // let pats = tree_from_pats("examples/ultrascale.tdl")?;
+    let pats = tree_from_pats("examples/ultrascale.tdl")?;
     let blks = tree_from_prog("examples/fsm.ir")?;
-    for b in blks {
-        let cuts = b.cut(0);
-        for c in cuts {
-            if let Some(node) = b.node(c) {
-                println!("start node:{}", node);
-                let cindex = b.dfg(c);
-                for ci in cindex {
-                    if let Some(vnode) = b.node(ci) {
-                        println!("{}", vnode);
-                    }
-                }
-                println!("\n")
+    for btree in blks {
+        let cuts = btree.cut(0);
+        for cut in cuts {
+            for (pname, ptree) in &pats {
+                println!("pat:{} is_match:{}", pname, is_match(&btree, &ptree, cut));
             }
-            // let ci = b.dfg(c);
-            // for (n, p) in &pats {
-            //     println!("Checking pat: {}", n);
-            //     let pi = p.dfg(0);
-            //     for (pii, cii) in pi.iter().zip(ci.iter()) {
-            //         if let Some(pnode) = p.node(*pii) {
-            //             if let Some(bnode) = b.node(*cii) {
-            //                 println!("pnode:{}   cnode:{}", pnode, bnode);
-            //             }
-            //         }
-            //     }
-            // }
         }
     }
     Ok(())
