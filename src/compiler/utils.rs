691ce883c120a34e657bab76c618aa65a265b7af
Luis Vega <vegaluisjose@gmail.com>
2021-02-07 13:40:11 -0800

    move func to utils

--- src/compiler/utils.rs
+++ src/compiler/utils.rs
@@ -1,5 +1,12 @@
+use crate::compiler::tree::{Forest, Tree};
 use crate::ir::ast::*;
+use crate::ir::parser::IRParser;
+use crate::tdl::ast::Pat;
+use crate::tdl::parser::TDLParser;
+use crate::util::errors::Error;
+use itertools::izip;
 use std::collections::HashMap;
+use std::convert::TryFrom;
 
 pub fn tree_roots_from_def(def: &Def) -> Vec<Id> {
     let mut count: HashMap<Id, u64> = HashMap::new();
@@ -39,4 +46,103 @@ pub fn tree_roots_from_def(def: &Def) -> Vec<Id> {
         }
     }
     root
+}
+
+pub fn tree_from_pats(pat_map: &HashMap<String, Pat>) -> Result<HashMap<String, Tree>, Error> {
+    let mut pattern: HashMap<String, Tree> = HashMap::new();
+    for (n, p) in pat_map {
+        pattern.insert(n.clone(), Tree::try_from(p.clone())?);
+    }
+    Ok(pattern)
+}
+
+pub fn tree_from_prog(file: &str) -> Result<Vec<Tree>, Error> {
+    let prog = IRParser::parse_from_file(file)?;
+    if let Some(main) = prog.get("main") {
+        let forest = Forest::try_from(main.clone())?;
+        Ok(forest.tree().clone())
+    } else {
+        Err(Error::new_conv_error("converting to tree"))
+    }
+}
+
+pub fn is_valid_change(block: &Tree, pat: &Tree, start: u64) -> (bool, u64) {
+    let pindex = pat.bfs(0);
+    let bindex = block.bfs_bound(start, pindex.len());
+    let mut is_match = true;
+    let mut bcost: u64 = 0;
+    if let Some(proot) = pat.node(0) {
+        let pcost = proot.cost();
+        for (p, b) in izip!(&pindex, &bindex) {
+            if let Some(pnode) = pat.node(*p) {
+                if let Some(bnode) = block.node(*b) {
+                    if pnode.ty() != bnode.ty()
+                        || (!pnode.is_inp() && pnode.op() != bnode.op())
+                        || (!pnode.is_inp()
+                            && !bnode.prim().is_any()
+                            && pnode.prim() != bnode.prim())
+                        || (!pnode.is_inp() && pnode.attr() != bnode.attr())
+                    {
+                        is_match = false;
+                    }
+                    if !pnode.is_inp() && bcost != u64::MAX {
+                        bcost += bnode.cost();
+                    }
+                }
+            }
+        }
+        (is_match & (pcost < bcost), pcost)
+    } else {
+        (false, u64::MAX)
+    }
+}
+
+pub fn tree_update(block: &Tree, pat: &Tree, target: u64, pat_name: &str, pat_cost: u64) -> Tree {
+    let pindex = pat.bfs(0);
+    let bindex = block.bfs_bound(target, pindex.len());
+    let mut btree = block.clone();
+    for (p, b) in izip!(&pindex, &bindex) {
+        if let Some(pnode) = pat.node(*p) {
+            if !pnode.is_inp() {
+                if let Some(bnode) = btree.node_mut(*b) {
+                    bnode.clear_pat();
+                }
+            }
+        }
+    }
+    if let Some(bnode) = btree.node_mut(target) {
+        bnode.set_pat(pat_name);
+        bnode.set_cost(pat_cost);
+    }
+    btree
+}
+
+pub fn tree_codegen(block: &Tree) {
+    for index in block.dfg(0) {
+        if let Some(node) = block.node(index) {
+            if let Some(name) = node.pat() {
+                println!("{}", name);
+            }
+        }
+    }
+}
+
+pub fn test() -> Result<(), Error> {
+    let blks = tree_from_prog("examples/fsm.ir")?;
+    let target = TDLParser::parse_from_file("examples/ultrascale.tdl")?;
+    let pats = tree_from_pats(target.pat())?;
+    for btree in blks {
+        let mut ctree = btree.clone();
+        let cuts = btree.cut(0);
+        for cut in cuts {
+            for (pname, ptree) in &pats {
+                let (is_valid, cost) = is_valid_change(&ctree, &ptree, cut);
+                if is_valid {
+                    ctree = tree_update(&ctree, &ptree, cut, &pname, cost);
+                }
+            }
+        }
+        tree_codegen(&ctree);
+    }
+    Ok(())
 }
