49bca1b3af47f475a75a8f6bdc105accee988b68
Luis Vega <vegaluisjose@gmail.com>
2021-02-15 17:15:35 -0800

    refactor

--- src/compiler/utils.rs
+++ src/compiler/utils.rs
@@ -2,7 +2,7 @@ use crate::asm::ast as asm;
 use crate::compiler::tree::{Forest, Tree};
 use crate::ir::ast as ir;
 use crate::ir::parser::IRParser;
-use crate::tdl::ast::{Pat, Target};
+use crate::tdl::ast::Pat;
 use crate::tdl::parser::TDLParser;
 use crate::util::errors::Error;
 use itertools::izip;
@@ -49,21 +49,28 @@ pub fn tree_roots_from_def(def: &ir::Def) -> Vec<ir::Id> {
     root
 }
 
-pub fn tree_from_pats(pat_map: &HashMap<String, Pat>) -> Result<HashMap<String, Tree>, Error> {
+pub fn tree_from_pats(pmap: &HashMap<String, Pat>) -> Result<HashMap<String, Tree>, Error> {
     let mut pattern: HashMap<String, Tree> = HashMap::new();
-    for (n, p) in pat_map {
+    for (n, p) in pmap {
         pattern.insert(n.clone(), Tree::try_from(p.clone())?);
     }
     Ok(pattern)
 }
 
-pub fn tree_from_prog(file: &str) -> Result<Vec<Tree>, Error> {
-    let prog = IRParser::parse_from_file(file)?;
+pub fn tree_from_prog(prog: &ir::Prog) -> Result<Vec<Tree>, Error> {
     if let Some(main) = prog.get("main") {
         let forest = Forest::try_from(main.clone())?;
         Ok(forest.tree().clone())
     } else {
         Err(Error::new_conv_error("converting to tree"))
+    }
+}
+
+pub fn imap_from_prog(prog: &ir::Prog) -> Result<ir::InstrMap, Error> {
+    if let Some(main) = prog.get("main") {
+        Ok(ir::InstrMap::from(main.clone()))
+    } else {
+        Err(Error::new_conv_error("converting to imap"))
     }
 }
 
@@ -112,6 +119,7 @@ pub fn tree_update(block: &Tree, pat: &Tree, target: u64, pat_name: &str, pat_co
                 if let Some(bnode) = btree.node_mut(*b) {
                     bnode.clear_pat();
                     bnode.set_cost(0);
+                    bnode.set_staged();
                 }
             }
         }
@@ -166,20 +174,39 @@ pub fn rename_expr(map: &HashMap<String, String>, input: &asm::Expr) -> Result<a
     }
 }
 
+pub fn tree_isel(blocks: &[Tree], pmap: &HashMap<String, Tree>) -> Result<Vec<Tree>, Error> {
+    let mut res: Vec<Tree> = Vec::new();
+    for btree in blocks {
+        let mut ctree = btree.clone();
+        let cuts = btree.cut(0);
+        for cut in cuts {
+            for (pname, ptree) in pmap {
+                let (is_valid, cost) = is_valid_change(&ctree, &ptree, cut);
+                if is_valid {
+                    ctree = tree_update(&ctree, &ptree, cut, &pname, cost);
+                }
+            }
+        }
+        res.push(ctree);
+    }
+    Ok(res)
+}
+
 pub fn tree_codegen(
+    imap: &ir::InstrMap,
     block: &Tree,
-    patmap: &HashMap<String, Tree>,
-    target: &Target,
+    tmap: &HashMap<String, Tree>,
+    pmap: &HashMap<String, Pat>,
 ) -> Result<(), Error> {
     for index in block.dfg(0) {
         if let Some(node) = block.node(index) {
             if let Some(name) = node.pat() {
-                if let Some(tree) = patmap.get(name) {
-                    if let Some(pat) = target.get_pat(&name) {
-                        let imap = input_map(block, tree, index);
-                        let omap = output_map(block, tree, index);
-                        let dst = rename_expr(&omap, pat.output())?;
-                        let arg = rename_expr(&imap, pat.input())?;
+                if let Some(tree) = tmap.get(name) {
+                    if let Some(pat) = pmap.get(name) {
+                        let input = input_map(block, tree, index);
+                        let output = output_map(block, tree, index);
+                        let dst = rename_expr(&output, pat.output())?;
+                        let arg = rename_expr(&input, pat.input())?;
                         let op = asm::OpAsm::from(name.clone());
                         let loc = asm::Loc {
                             prim: pat.prim().clone(),
@@ -190,6 +217,10 @@ pub fn tree_codegen(
                         println!("{}", asm);
                     }
                 }
+            } else if !node.is_staged() && node.is_wire() {
+                if let Some(instr) = imap.get(&node.id()) {
+                    println!("{}", instr);
+                }
             }
         }
     }
@@ -197,21 +228,14 @@ pub fn tree_codegen(
 }
 
 pub fn test() -> Result<(), Error> {
-    let blks = tree_from_prog("examples/fsm.ir")?;
-    let target = TDLParser::parse_from_file("examples/ultrascale.tdl")?;
-    let pats = tree_from_pats(target.pat())?;
-    for btree in blks {
-        let mut ctree = btree.clone();
-        let cuts = btree.cut(0);
-        for cut in cuts {
-            for (pname, ptree) in &pats {
-                let (is_valid, cost) = is_valid_change(&ctree, &ptree, cut);
-                if is_valid {
-                    ctree = tree_update(&ctree, &ptree, cut, &pname, cost);
-                }
-            }
-        }
-        tree_codegen(&ctree, &pats, &target)?;
+    let prog = IRParser::parse_from_file("examples/fsm.ir")?;
+    let tlut = TDLParser::parse_from_file("examples/ultrascale.tdl")?;
+    let lmap = tree_from_pats(tlut.pat())?;
+    let imap = imap_from_prog(&prog)?;
+    let blks = tree_from_prog(&prog)?;
+    let blks = tree_isel(&blks, &lmap)?;
+    for blk in blks {
+        tree_codegen(&imap, &blk, &lmap, tlut.pat())?;
     }
     Ok(())
 }
