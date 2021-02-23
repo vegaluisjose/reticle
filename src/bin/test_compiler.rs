97909e3c42ca04a47e43f1f32c120414c54a9b6c
Luis Vega <vegaluisjose@gmail.com>
2021-02-23 14:21:45 -0800

    expand test compiler

--- src/bin/test_compiler.rs
+++ src/bin/test_compiler.rs
@@ -2,6 +2,7 @@ use reticle::compiler::select;
 use reticle::ir::parser::IRParser;
 use reticle::placer::place_lut_from_prog;
 use reticle::util::errors::Error;
+use reticle::verilog::ast as verilog;
 use reticle::xl::ast as xl;
 use std::convert::TryFrom;
 
@@ -10,9 +11,11 @@ fn main() -> Result<(), Error> {
     let asm = select(&prog)?;
     let placed = place_lut_from_prog(&asm)?;
     let xl = xl::Prog::try_from(placed.clone())?;
+    let module = verilog::Module::try_from(xl.clone())?;
     println!("---ir---\n{}", prog);
     println!("---asm---\n{}", asm);
     println!("---placed---\n{}", placed);
     println!("---xl---\n{}", xl);
+    println!("---verilog---\n{}", module);
     Ok(())
 }
