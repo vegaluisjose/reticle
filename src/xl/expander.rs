9a3d87e2393bd704958af0853884d58ada606202
Luis Vega <vegaluisjose@gmail.com>
2021-02-23 14:08:01 -0800

    emit const in expander

--- src/xl/expander.rs
+++ src/xl/expander.rs
@@ -1,6 +1,7 @@
 use crate::asm::ast as asm;
 use crate::util::errors::Error;
 use crate::xl::ast as xl;
+use std::collections::HashMap;
 
 #[derive(Clone, Debug)]
 pub struct Expander {
@@ -8,15 +9,17 @@ pub struct Expander {
     pub prefix: String,
     pub sig: xl::Sig,
     pub body: Vec<xl::Instr>,
+    pub map: HashMap<String, String>,
 }
 
 impl Default for Expander {
     fn default() -> Self {
         Expander {
             count: 0,
-            prefix: String::new(),
+            prefix: "t".to_string(),
             sig: xl::Sig::default(),
             body: Vec::new(),
+            map: HashMap::new(),
         }
     }
 }
@@ -28,10 +31,24 @@ impl Expander {
     pub fn body(&self) -> &Vec<xl::Instr> {
         &self.body
     }
+    pub fn get_name(&self, key: &str) -> Option<&String> {
+        self.map.get(key)
+    }
+    pub fn new_name(&mut self) -> String {
+        let tmp = self.count;
+        self.count += 1;
+        format!("{}{}", self.prefix, tmp)
+    }
+    pub fn rename(&mut self, name: &str) -> String {
+        let tmp = self.new_name();
+        self.map.insert(name.to_string(), tmp.clone());
+        tmp
+    }
     pub fn expand_const(&mut self, instr: &asm::InstrWire) -> Result<(), Error> {
         let attr_term = instr.attr().get_term(0)?;
         let value = attr_term.get_val()?;
         let dst_term = instr.dst().get_term(0)?;
+        let mut arg_tup = xl::ExprTup::default();
         if let Some(width) = dst_term.width() {
             for i in 0..width {
                 let lsb = value >> i;
@@ -41,13 +58,35 @@ impl Expander {
                 } else {
                     xl::OpBasc::Gnd
                 };
+                let name = if width == 1 {
+                    self.rename(&dst_term.get_id()?)
+                } else {
+                    self.new_name()
+                };
+                let ty = xl::Ty::Bool;
+                let term = xl::ExprTerm::Var(name, ty);
+                let dst = xl::Expr::from(term.clone());
+                arg_tup.add_term(term);
                 let instr_basc = xl::InstrBasc {
                     op,
                     attr: xl::Expr::default(),
-                    dst: xl::Expr::default(),
+                    dst,
                     arg: xl::Expr::default(),
                 };
                 self.add_instr(xl::Instr::from(instr_basc));
+            }
+            if width > 1 {
+                let dst_id = self.rename(&dst_term.get_id()?);
+                let dst_ty = dst_term.get_ty()?;
+                let dst = xl::Expr::from(xl::ExprTerm::Var(dst_id, dst_ty.clone()));
+                let arg = xl::Expr::from(arg_tup);
+                let cat = xl::InstrBasc {
+                    op: xl::OpBasc::Cat,
+                    attr: xl::Expr::default(),
+                    dst,
+                    arg,
+                };
+                self.add_instr(xl::Instr::from(cat));
             }
         }
         Ok(())
