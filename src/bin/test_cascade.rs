cfcd8aa0eac59e7fec0f207aad2c428820fde3e7
Luis Vega <vegaluisjose@gmail.com>
2021-03-02 09:29:07 -0800

    update cascade test

--- src/bin/test_cascade.rs
+++ src/bin/test_cascade.rs
@@ -1,11 +1,23 @@
-use reticle::asm::parser::AsmParser;
+use reticle::asm::ast as asm;
+use reticle::ir::parser::IRParser;
 use reticle::optimizer::cascader::cascade;
+use reticle::placer::place_from_prog;
 use reticle::util::errors::Error;
+use reticle::verilog::ast as verilog;
+use reticle::xl::ast as xl;
+use std::convert::TryFrom;
 
 fn main() -> Result<(), Error> {
-    let p0 = AsmParser::parse_from_file("examples/asm/tdot.asm")?;
-    let p1 = cascade(&p0)?;
-    println!("input\n{}\n", p0);
-    println!("output\n{}\n", p1);
+    let prog = IRParser::parse_from_file("examples/ir/tdot.ir")?;
+    let asm = asm::Prog::try_from(prog.clone())?;
+    let asm = cascade(&asm)?;
+    let placed = place_from_prog(&asm)?;
+    let xl = xl::Prog::try_from(placed.clone())?;
+    let module = verilog::Module::try_from(xl.clone())?;
+    println!("---ir---\n{}", prog);
+    println!("---asm---\n{}", asm);
+    println!("---placed---\n{}", placed);
+    println!("---xl---\n{}", xl);
+    println!("---verilog---\n{}", module);
     Ok(())
 }
