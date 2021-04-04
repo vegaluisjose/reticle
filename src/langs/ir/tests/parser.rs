4182a63db4a5bbefb65471b4756274343b5b08c5
Luis Vega <vegaluisjose@gmail.com>
2021-04-04 13:22:12 -0700

    add ir tests

---
+++ src/langs/ir/tests/parser.rs
@@ -0,0 +1,12 @@
+use ir::ast::*;
+use ir::parser::Parser;
+
+#[test]
+fn simple_def() {
+    let res = Parser::parse_from_str("def main () -> () {}").unwrap();
+    let mut def = Def::default();
+    def.set_id("main");
+    let mut exp = Prog::default();
+    exp.insert(&def.id(), def);
+    assert_eq!(res, exp);
+}
