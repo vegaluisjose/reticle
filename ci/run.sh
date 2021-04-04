2bbbf573c771ccb79cab2a3efed55b0435fff05b
Luis Vega <vegaluisjose@gmail.com>
2021-04-04 12:25:52 -0700

    test ci

--- ci/run.sh
+++ ci/run.sh
@@ -2,5 +2,12 @@
 
 set -eo pipefail
 
-cargo fmt -- --check
-cargo clippy --all-targets --all-features -- -D warnings
+# cargo fmt -- --check
+# cargo clippy --all-targets --all-features -- -D warnings
+
+# create a directory for cargo registry, so
+# dependencies can be cached when running rust
+# inside docker
+mkdir -p $PWD/.cargo/registry
+
+source $PWD/docker/install_rust.sh
