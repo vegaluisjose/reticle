5c345a19ad608d1c39398851110c7aeb7ba2b8f2
Luis Vega <vegaluisjose@gmail.com>
2021-04-04 12:39:56 -0700

    test docker command

--- ci/run.sh
+++ ci/run.sh
@@ -5,9 +5,16 @@ set -eo pipefail
 # cargo fmt -- --check
 # cargo clippy --all-targets --all-features -- -D warnings
 
+DOCKER_RETICLE_IMG_NAME="reticle-rust"
+
 # create a directory for cargo registry, so
 # dependencies can be cached when running rust
 # inside docker
 mkdir -p $PWD/.cargo/registry
 
-docker build -t "reticle-rust" -f $PWD/docker/Dockerfile.rust .
+docker build -t $DOCKER_RETICLE_IMG_NAME -f $PWD/docker/Dockerfile.rust .
+
+docker run --rm --pid=host \
+    --user "$(id -u):$(id -g)"
+    $DOCKER_RETICLE_IMG_NAME
+    "ls"
