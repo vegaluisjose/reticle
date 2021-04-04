2e752966895c92040b8473d6b00d7fec40ed16aa
Luis Vega <vegaluisjose@gmail.com>
2021-04-04 12:56:14 -0700

    test ci new rust version

--- ci/run.sh
+++ ci/run.sh
@@ -7,11 +7,13 @@ set -eo pipefail
 
 DOCKER_RUST_IMG_NAME="reticle-rust"
 DOCKER_RUST_APP_PATH="/usr/src/myapp"
+DOCKER_RUST_REG_PATH="/usr/local/cargo/registry"
+LOCAL_RUST_REG_PATH="$PWD/.cargo/registry"
 
 # create a directory for cargo registry, so
 # dependencies can be cached when running rust
 # inside docker
-mkdir -p $PWD/.cargo/registry
+mkdir -p $LOCAL_RUST_REG_PATH
 
 docker build -t $DOCKER_RUST_IMG_NAME -f $PWD/docker/Dockerfile.rust .
 
@@ -19,6 +21,7 @@ function docker_rust {
     docker run --rm --pid=host \
         --user "$(id -u):$(id -g)" \
         -v "$PWD:$DOCKER_RUST_APP_PATH" \
+        -v "$LOCAL_RUST_REG_PATH:$DOCKER_RUST_REG_PATH"
         -w "$DOCKER_RUST_APP_PATH" \
         $DOCKER_RUST_IMG_NAME \
         $1
