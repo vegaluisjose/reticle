8aeb494041e092bb2e636ca33a78450b5a7ae390
Luis Vega <vegaluisjose@gmail.com>
2021-04-04 12:51:25 -0700

    test ci function

--- ci/run.sh
+++ ci/run.sh
@@ -15,9 +15,14 @@ mkdir -p $PWD/.cargo/registry
 
 docker build -t $DOCKER_RUST_IMG_NAME -f $PWD/docker/Dockerfile.rust .
 
-docker run --rm --pid=host \
-    --user "$(id -u):$(id -g)" \
-    -v "$PWD:$DOCKER_RUST_APP_PATH" \
-    -w "$DOCKER_RUST_APP_PATH" \
-    $DOCKER_RUST_IMG_NAME \
-    cargo --version
+function docker_rust {
+    docker run --rm --pid=host \
+        --user "$(id -u):$(id -g)" \
+        -v "$PWD:$DOCKER_RUST_APP_PATH" \
+        -w "$DOCKER_RUST_APP_PATH" \
+        $DOCKER_RUST_IMG_NAME \
+        $1
+}
+
+docker_rust "cargo --version"
+docker_rust "rustc --version"
