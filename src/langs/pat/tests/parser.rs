d31d4b64a2d4477cc6c3a7ed6b4c5d6d8e5aaa98
Luis Vega <vegaluisjose@gmail.com>
2021-04-05 09:02:40 -0700

    add one pat test

---
+++ src/langs/pat/tests/parser.rs
@@ -0,0 +1,14 @@
+use pat::ast::*;
+use pat::parser::Parser;
+
+#[test]
+fn simple_prog() {
+    let res = Parser::parse_from_str("pat add_i8_0 () -> () {}").unwrap();
+    let mut sig = Sig::default();
+    sig.set_id("add_i8_0");
+    let mut pat = Pat::default();
+    pat.set_sig(sig);
+    let mut exp = Target::default();
+    exp.insert(&pat.id(), pat);
+    assert_eq!(res, exp);
+}
