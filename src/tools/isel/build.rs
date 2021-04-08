d3aa94a4c224a8be855e231832ed2ab5a36ad26d
Luis Vega <vegaluisjose@gmail.com>
2021-04-08 10:21:04 -0700

    fmt

--- src/tools/isel/build.rs
+++ src/tools/isel/build.rs
@@ -14,7 +14,10 @@ fn build_pattern(prim: &str) {
 fn build_implementation(prim: &str) {
     use xim::parser::Parser;
     let out_dir = env::var("OUT_DIR").unwrap();
-    let imp_path = format!("../../../examples/target/implementations/ultrascale/{}.xim", prim);
+    let imp_path = format!(
+        "../../../examples/target/implementations/ultrascale/{}.xim",
+        prim
+    );
     let bin_name = format!("{}_xim.bin", prim);
     let imp = Parser::parse_from_file(imp_path).unwrap();
     let bin_path = Path::new(&out_dir).join(bin_name);
