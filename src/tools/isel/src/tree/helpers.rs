2e8f17c24f934040c0289b16b3c9385b10a63f7c
Luis Vega <vegaluisjose@gmail.com>
2021-04-08 20:18:00 -0700

    add tree helpers

--- src/tools/isel/src/tree/helpers.rs
+++ src/tools/isel/src/tree/helpers.rs
@@ -1,5 +1,8 @@
+use crate::errors::Error;
 use crate::tree::*;
 use std::collections::HashMap;
+use std::collections::VecDeque;
+use std::convert::TryFrom;
 
 impl Node {
     pub fn index(&self) -> u64 {
@@ -67,6 +70,146 @@ impl Node {
     }
     pub fn clear_pat(&mut self) {
         self.pat = None;
+    }
+}
+
+impl Tree {
+    pub fn index(&self) -> u64 {
+        self.index
+    }
+    pub fn node_map(&self) -> &HashMap<u64, Node> {
+        &self.node
+    }
+    pub fn edge_map(&self) -> &HashMap<u64, Vec<u64>> {
+        &self.edge
+    }
+    pub fn node(&self, index: u64) -> Option<&Node> {
+        self.node.get(&index)
+    }
+    pub fn edge(&self, index: u64) -> Option<&Vec<u64>> {
+        self.edge.get(&index)
+    }
+    pub fn dfg(&self, start: u64) -> Vec<u64> {
+        let mut res: Vec<u64> = Vec::new();
+        let mut stack: Vec<u64> = Vec::new();
+        stack.push(start);
+        while let Some(cur) = stack.pop() {
+            res.push(cur);
+            if let Some(edge) = self.edge(cur) {
+                for e in edge {
+                    stack.push(*e);
+                }
+            }
+        }
+        res
+    }
+    pub fn bfs(&self, start: u64) -> Vec<u64> {
+        let mut res: Vec<u64> = Vec::new();
+        let mut stack: VecDeque<u64> = VecDeque::new();
+        stack.push_back(start);
+        while let Some(cur) = stack.pop_front() {
+            res.push(cur);
+            if let Some(edge) = self.edge(cur) {
+                for e in edge {
+                    stack.push_back(*e);
+                }
+            }
+        }
+        res
+    }
+    pub fn bfs_bound(&self, start: u64, len: usize) -> Vec<u64> {
+        let mut res: Vec<u64> = Vec::new();
+        let mut stack: VecDeque<u64> = VecDeque::new();
+        stack.push_back(start);
+        while let Some(cur) = stack.pop_front() {
+            res.push(cur);
+            if let Some(edge) = self.edge(cur) {
+                for e in edge {
+                    if stack.len() < len {
+                        stack.push_back(*e);
+                    }
+                }
+            }
+        }
+        res
+    }
+    pub fn cut(&self, start: u64) -> Vec<u64> {
+        let mut res: Vec<u64> = Vec::new();
+        let mut stack: Vec<u64> = Vec::new();
+        stack.push(start);
+        while let Some(cur) = stack.pop() {
+            if let Some(node) = self.node.get(&cur) {
+                if node.is_prim() && !node.is_committed() {
+                    res.push(cur);
+                }
+            }
+            if let Some(edge) = self.edge(cur) {
+                for e in edge {
+                    stack.push(*e);
+                }
+            }
+        }
+        res.reverse();
+        res
+    }
+    pub fn node_mut(&mut self, index: u64) -> Option<&mut Node> {
+        self.node.get_mut(&index)
+    }
+    pub fn new_index(&mut self) -> u64 {
+        let curr = self.index;
+        self.index += 1;
+        curr
+    }
+    pub fn add_node(&mut self, instr: &Instr) -> Result<u64, Error> {
+        let curr = self.new_index();
+        let mut node = Node::try_from(instr.clone())?;
+        node.set_index(curr);
+        self.node.insert(curr, node);
+        self.edge.insert(curr, vec![]);
+        Ok(curr)
+    }
+    pub fn add_node_with_cost(&mut self, instr: &Instr, cost: u64) -> Result<u64, Error> {
+        let curr = self.new_index();
+        let mut node = Node::try_from(instr.clone())?;
+        node.set_index(curr);
+        node.set_cost(cost);
+        self.node.insert(curr, node);
+        self.edge.insert(curr, vec![]);
+        Ok(curr)
+    }
+    pub fn add_input(&mut self, id: &str, ty: Ty) -> u64 {
+        let op = NodeOp::Inp;
+        let curr = self.new_index();
+        let mut node = Node {
+            index: curr,
+            id: id.to_string(),
+            ty,
+            op,
+            attr: Expr::default(),
+            prim: Prim::Any,
+            cost: 0,
+            staged: false,
+            committed: false,
+            pat: None,
+        };
+        node.set_index(curr);
+        self.node.insert(curr, node);
+        curr
+    }
+    pub fn add_edge(&mut self, from: u64, to: u64) {
+        if let Some(edges) = self.edge.get_mut(&from) {
+            edges.push(to);
+        }
+    }
+    pub fn commit(&mut self) {
+        let index = self.bfs(0);
+        for i in index {
+            if let Some(node) = self.node_mut(i) {
+                if !node.is_inp() && node.pat().is_some() && node.is_staged() {
+                    node.commit();
+                }
+            }
+        }
     }
 }
 
