ab5a3d9c14fa5f13f545a3b9c1c2fa7130697a8b
Luis Vega <vegaluisjose@gmail.com>
2021-04-20 00:43:45 +0000

    update example

---
+++ examples/asm/tadd.asm
@@ -0,0 +1,18 @@
+def main(en:bool, a0:i8<4>, b0:i8<4>, a1:i8<4>, b1:i8<4>, a2:i8<4>, b2:i8<4>, a3:i8<4>, b3:i8<4>, a4:i8<4>, b4:i8<4>, a5:i8<4>, b5:i8<4>, a6:i8<4>, b6:i8<4>, a7:i8<4>, b7:i8<4>, a8:i8<4>, b8:i8<4>, a9:i8<4>, b9:i8<4>, a10:i8<4>, b10:i8<4>, a11:i8<4>, b11:i8<4>, a12:i8<4>, b12:i8<4>, a13:i8<4>, b13:i8<4>, a14:i8<4>, b14:i8<4>, a15:i8<4>, b15:i8<4>) -> (y0:i8<4>, y1:i8<4>, y2:i8<4>, y3:i8<4>, y4:i8<4>, y5:i8<4>, y6:i8<4>, y7:i8<4>, y8:i8<4>, y9:i8<4>, y10:i8<4>, y11:i8<4>, y12:i8<4>, y13:i8<4>, y14:i8<4>, y15:i8<4>) {
+    y0:i8<4> = daddrega_i8v4(a0, b0, en) @dsp(??, ??);
+    y1:i8<4> = daddrega_i8v4(a1, b1, en) @dsp(??, ??);
+    y2:i8<4> = daddrega_i8v4(a2, b2, en) @dsp(??, ??);
+    y3:i8<4> = daddrega_i8v4(a3, b3, en) @dsp(??, ??);
+    y4:i8<4> = daddrega_i8v4(a4, b4, en) @dsp(??, ??);
+    y5:i8<4> = daddrega_i8v4(a5, b5, en) @dsp(??, ??);
+    y6:i8<4> = daddrega_i8v4(a6, b6, en) @dsp(??, ??);
+    y7:i8<4> = daddrega_i8v4(a7, b7, en) @dsp(??, ??);
+    y8:i8<4> = daddrega_i8v4(a8, b8, en) @dsp(??, ??);
+    y9:i8<4> = daddrega_i8v4(a9, b9, en) @dsp(??, ??);
+    y10:i8<4> = daddrega_i8v4(a10, b10, en) @dsp(??, ??);
+    y11:i8<4> = daddrega_i8v4(a11, b11, en) @dsp(??, ??);
+    y12:i8<4> = daddrega_i8v4(a12, b12, en) @dsp(??, ??);
+    y13:i8<4> = daddrega_i8v4(a13, b13, en) @dsp(??, ??);
+    y14:i8<4> = daddrega_i8v4(a14, b14, en) @dsp(??, ??);
+    y15:i8<4> = daddrega_i8v4(a15, b15, en) @dsp(??, ??);
+}
