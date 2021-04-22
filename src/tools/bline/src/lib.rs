1ea8fb22aa4943bdd1a22f397b89803586ff5dc0
Luis Vega <vegaluisjose@gmail.com>
2021-04-22 19:27:58 +0000

    impl signature name generation

--- src/tools/bline/src/lib.rs
+++ src/tools/bline/src/lib.rs
@@ -1,1 +1,19 @@
 pub mod errors;
+
+use crate::errors::Error;
+use ir::ast as ir;
+use verilog::ast as vl;
+
+pub fn try_from_ir_def(def: &ir::Def) -> Result<vl::Module, Error> {
+    let id = def.sig().id();
+    let module = vl::Module::new(&id);
+    Ok(module)
+}
+
+pub fn try_from_ir_prog(prog: &ir::Prog) -> Result<vl::Module, Error> {
+    if let Some(def) = prog.get("main") {
+        Ok(try_from_ir_def(def)?)
+    } else {
+        Err(Error::new_bline_error("main not found"))
+    }
+}
