4594bd4e6089c4df1a29aef6a4b5de2d0373aec0
Luis Vega <vegaluisjose@gmail.com>
2021-04-23 21:25:12 +0000

    start working on expander

---
+++ src/tools/xpand/src/errors.rs
@@ -0,0 +1,47 @@
+use xir::errors::Error as XirError;
+use ir::errors::Error as IRError;
+use std::fmt;
+use std::num::TryFromIntError;
+
+#[derive(Debug)]
+pub enum Error {
+    IR(IRError),
+    Xir(XirError),
+    Xpand(String),
+    TryFromInt(TryFromIntError),
+}
+
+impl Error {
+    pub fn new_xpand_error(msg: &str) -> Self {
+        Error::Xpand(msg.to_string())
+    }
+}
+
+impl From<IRError> for Error {
+    fn from(e: IRError) -> Self {
+        Error::IR(e)
+    }
+}
+
+impl From<XirError> for Error {
+    fn from(e: XirError) -> Self {
+        Error::Xir(e)
+    }
+}
+
+impl From<TryFromIntError> for Error {
+    fn from(e: TryFromIntError) -> Self {
+        Error::TryFromInt(e)
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Error::IR(msg) => write!(f, "{}", msg),
+            Error::Xir(msg) => write!(f, "{}", msg),
+            Error::Xpand(msg) => write!(f, "{}", msg),
+            Error::TryFromInt(msg) => write!(f, "{}", msg),
+        }
+    }
+}
---
+++ src/tools/xpand/src/lib.rs
@@ -0,0 +1,6 @@
+pub mod errors;
+
+pub const CLOCK: &str = "clock";
+pub const RESET: &str = "reset";
+pub const VCC: &str = "vcc";
+pub const GND: &str = "gnd";
