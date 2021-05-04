a035ce4246e79f51291251e34fbc72b84998b66e
Luis Vega <vegaluisjose@gmail.com>
2021-05-04 17:35:03 +0000

    add remaining helpers to lut2

--- src/tools/xpand/src/lut.rs
+++ src/tools/xpand/src/lut.rs
@@ -1,9 +1,7 @@
-// use crate::loc::attr_from_loc;
-// use crate::loc::{Bel, BelLut, ExprCoord, Loc};
-// use std::collections::HashMap;
-use crate::loc::Loc;
+use crate::loc::attr_from_loc;
+use crate::loc::{Bel, BelLut, ExprCoord, Loc};
 use crate::port::{Input, Output};
-// use verilog::ast as vl;
+use verilog::ast as vl;
 
 #[derive(Clone, Debug)]
 pub struct Attr {
@@ -23,5 +21,45 @@ pub struct Lut2 {
 impl Default for Attr {
     fn default() -> Self {
         Attr { init: 0 }
+    }
+}
+
+impl Default for Lut2 {
+    fn default() -> Self {
+        let loc = Loc {
+            bel: Bel::Lut(BelLut::A6),
+            x: ExprCoord::default(),
+            y: ExprCoord::default(),
+        };
+        Lut2 {
+            name: String::new(),
+            prim: "LUT2".to_string(),
+            loc,
+            attr: Attr::default(),
+            input: Input::lut2(),
+            output: Output::lut(),
+        }
+    }
+}
+
+impl Lut2 {
+    pub fn to_instance(&self) -> vl::Instance {
+        let mut inst = vl::Instance::new(&self.name, &self.prim);
+        let init = format!("{:X}", self.attr.init);
+        inst.add_param("INIT", vl::Expr::new_ulit_hex(4, &init));
+        for (k, v) in self.input.connection.iter() {
+            inst.connect(&k, v.clone());
+        }
+        for (k, v) in self.output.connection.iter() {
+            inst.connect(&k, v.clone());
+        }
+        if self.loc.is_placed() {
+            let attr = attr_from_loc(&self.loc);
+            inst.set_attr(attr);
+        }
+        inst
+    }
+    pub fn set_name(&mut self, name: &str) {
+        self.name = name.to_string();
     }
 }
--- src/tools/xpand/src/main.rs
+++ src/tools/xpand/src/main.rs
@@ -1,7 +1,7 @@
-use xpand::dsp::Dsp;
+use xpand::lut::Lut2;
 
 pub fn main() {
-    let mut dsp = Dsp::default();
-    dsp.set_name("i0");
-    println!("{}", dsp.to_instance());
+    let mut lut = Lut2::default();
+    lut.set_name("i0");
+    println!("{}", lut.to_instance());
 }
