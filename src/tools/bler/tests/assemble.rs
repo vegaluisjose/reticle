da5c4dc3ec86cbde11e541aa243656d62346e0b4
Luis Vega <vegaluisjose@gmail.com>
2021-05-28 00:14:23 +0000

    add assembler tests

--- src/tools/bler/tests/assemble.rs
+++ src/tools/bler/tests/assemble.rs
@@ -1,19 +1,45 @@
 use asm::parser::Parser as AsmParser;
 use bler::try_from_asm_prog;
 use xir::parser::Parser as XirParser;
+use std::path::Path;
 
-#[test]
-fn add() {
-    let prog = AsmParser::parse_from_file("../../../examples/asm/add.asm").unwrap();
-    let exp = XirParser::parse_from_file("../../../examples/xir/add.xir").unwrap();
-    let res = try_from_asm_prog(&prog).unwrap();
+fn test(name: &str) {
+    let mut i = Path::new("../../../examples/asm").join(name);
+    let mut o = Path::new("../../../examples/xir").join(name);
+    i.set_extension("asm");
+    o.set_extension("xir");
+    let p = AsmParser::parse_from_file(i).unwrap();
+    let exp = XirParser::parse_from_file(o).unwrap();
+    let res = try_from_asm_prog(&p).unwrap();
     assert_eq!(res, exp);
 }
 
 #[test]
+fn add() {
+    test("add");
+}
+
+#[test]
 fn addreduce_placed() {
-    let prog = AsmParser::parse_from_file("../../../examples/asm/addreduce_placed.asm").unwrap();
-    let exp = XirParser::parse_from_file("../../../examples/xir/addreduce_placed.xir").unwrap();
-    let res = try_from_asm_prog(&prog).unwrap();
-    assert_eq!(res, exp);
+    test("addreduce_placed");
+}
+
+#[test]
+fn fsm_3() {
+    test("fsm_3");
+}
+
+#[test]
+fn fsm_5() {
+    test("fsm_5");
+}
+
+#[test]
+fn fsm_7() {
+    test("fsm_7");
+}
+
+#[test]
+fn fsm_9() {
+    test("fsm_9");
 }
