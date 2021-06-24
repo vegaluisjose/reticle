2c13b2918b36ade3d1eb1abf4ad0275641eb934f
Luis Vega <vegaluisjose@gmail.com>
2021-06-24 19:23:18 +0000

    add lut tests

--- src/tools/xpand/tests/prim.rs
+++ src/tools/xpand/tests/prim.rs
@@ -3,6 +3,7 @@ use xpand::fdre::Fdre;
 use xpand::fdse::Fdse;
 use xpand::gnd::Gnd;
 use xpand::instance::ToInstance;
+use xpand::lut::{Lut1, Lut2, Lut3, Lut4, Lut5, Lut6};
 use xpand::vcc::Vcc;
 
 fn test<S: AsRef<str>>(res: impl ToInstance, exp: S) -> Result<(), Error> {
@@ -60,4 +61,85 @@ fn test_vcc() -> Result<(), Error> {
     .P(vcc)
 );"#;
     test(Vcc::default(), exp)
+}
+
+#[test]
+fn test_lut1() -> Result<(), Error> {
+    let exp = r#"LUT1 # (
+    .INIT(1'h0)
+)  (
+    .I0(gnd),
+    .O()
+);"#;
+    test(Lut1::default(), exp)
+}
+
+#[test]
+fn test_lut2() -> Result<(), Error> {
+    let exp = r#"LUT2 # (
+    .INIT(4'h0)
+)  (
+    .I0(gnd),
+    .I1(gnd),
+    .O()
+);"#;
+    test(Lut2::default(), exp)
+}
+
+#[test]
+fn test_lut3() -> Result<(), Error> {
+    let exp = r#"LUT3 # (
+    .INIT(8'h0)
+)  (
+    .I0(gnd),
+    .I1(gnd),
+    .I2(gnd),
+    .O()
+);"#;
+    test(Lut3::default(), exp)
+}
+
+#[test]
+fn test_lut4() -> Result<(), Error> {
+    let exp = r#"LUT4 # (
+    .INIT(16'h0)
+)  (
+    .I0(gnd),
+    .I1(gnd),
+    .I2(gnd),
+    .I3(gnd),
+    .O()
+);"#;
+    test(Lut4::default(), exp)
+}
+
+#[test]
+fn test_lut5() -> Result<(), Error> {
+    let exp = r#"LUT5 # (
+    .INIT(32'h0)
+)  (
+    .I0(gnd),
+    .I1(gnd),
+    .I2(gnd),
+    .I3(gnd),
+    .I4(gnd),
+    .O()
+);"#;
+    test(Lut5::default(), exp)
+}
+
+#[test]
+fn test_lut6() -> Result<(), Error> {
+    let exp = r#"LUT6 # (
+    .INIT(64'h0)
+)  (
+    .I0(gnd),
+    .I1(gnd),
+    .I2(gnd),
+    .I3(gnd),
+    .I4(gnd),
+    .I5(gnd),
+    .O()
+);"#;
+    test(Lut6::default(), exp)
 }
