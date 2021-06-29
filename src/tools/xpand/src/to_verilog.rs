0dd522ca2c057272be3799578f0ea27d0c4e0213
Luis Vega <vegaluisjose@gmail.com>
2021-06-29 18:30:32 +0000

    [tools][xpand] add two more methods for ToVerilog trait

--- src/tools/xpand/src/to_verilog.rs
+++ src/tools/xpand/src/to_verilog.rs
@@ -6,9 +6,16 @@ pub type ExprMap = HashMap<String, vl::Expr>;
 /// TODO: ToVerilog trait should replace Instance trait
 ///
 /// ToVerilog trait contains the function required to
-/// produce block of Verilog code. All the methods in
-/// this trait are optional
+/// produce block of Verilog code, containing at most
+/// one instance and multiple statements. All methods
+/// in this trait are optional
 pub trait ToVerilog {
+    fn to_name(&self) -> String {
+        String::new()
+    }
+    fn to_prim(&self) -> String {
+        String::new()
+    }
     fn to_expr(&self) -> vl::Expr {
         vl::Expr::new_str("")
     }
@@ -22,7 +29,12 @@ pub trait ToVerilog {
         ExprMap::new()
     }
     fn to_instance(&self) -> vl::Instance {
-        vl::Instance::new("", "")
+        let param = self.to_param();
+        let mut inst = vl::Instance::new(&self.to_name(), &self.to_prim());
+        for (p, v) in param {
+            inst.add_param(&p, v);
+        }
+        inst
     }
     fn to_stmt(&self) -> vl::Stmt {
         vl::Stmt::from(self.to_instance())
