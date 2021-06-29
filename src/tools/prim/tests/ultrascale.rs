92985346d22b97c7fedacda46af13818cf74b247
Luis Vega <vegaluisjose@gmail.com>
2021-06-29 18:27:39 +0000

    [tools][prim] fix ultrascale tests

--- src/tools/prim/tests/ultrascale.rs
+++ src/tools/prim/tests/ultrascale.rs
@@ -1,30 +1,31 @@
 use anyhow::Result;
 use prim::ultrascale::carry::{Carry, CarryParam, CarryType};
-use prim::{Param, ParamSet, PortSet, ToPrim};
+use prim::{Param, ParamSet, PortSet, Prim};
+use std::fmt;
 use std::iter::FromIterator;
 
-fn test_name<T, U: ToPrim<T>>(prim: &U, exp: &str) {
-    let res = prim.to_name();
+fn test_name<T: Eq + Default + fmt::Debug + fmt::Display>(prim: &Prim<T>, exp: &str) {
+    let res = prim.name();
     assert_eq!(res, exp);
 }
 
-fn test_param<T: std::fmt::Debug, U: ToPrim<T>>(prim: &U, exp: &ParamSet<T>) {
-    let res = prim.to_param();
+fn test_param<T: Eq + Default + fmt::Debug + fmt::Display>(prim: &Prim<T>, exp: &ParamSet<T>) {
+    let res = prim.param();
     let res = Vec::from_iter(res.iter());
     let exp = Vec::from_iter(exp.iter());
     assert_eq!(res, exp);
 }
 
-fn test_input<T, U: ToPrim<T>>(prim: &U, exp: &[(&str, u32)]) {
-    let res = prim.to_input();
+fn test_input<T: Eq + Default + fmt::Debug + fmt::Display>(prim: &Prim<T>, exp: &[(&str, u32)]) {
+    let res = prim.input();
     let exp = PortSet::from(exp);
-    assert_eq!(res, exp);
+    assert_eq!(*res, exp);
 }
 
-fn test_output<T, U: ToPrim<T>>(prim: &U, exp: &[(&str, u32)]) {
-    let res = prim.to_output();
+fn test_output<T: Eq + Default + fmt::Debug + fmt::Display>(prim: &Prim<T>, exp: &[(&str, u32)]) {
+    let res = prim.output();
     let exp = PortSet::from(exp);
-    assert_eq!(res, exp);
+    assert_eq!(*res, exp);
 }
 
 #[test]
@@ -61,8 +62,7 @@ fn test_carry_output() {
 
 #[test]
 fn test_carry_set_param() -> Result<()> {
-    let carry = Carry::default();
-    let mut prim = carry.to_prim();
-    prim.set_param("CARRY_TYPE", CarryType::Dual)?;
+    let mut carry = Carry::default();
+    carry.set_param("CARRY_TYPE", CarryType::Dual)?;
     Ok(())
 }
