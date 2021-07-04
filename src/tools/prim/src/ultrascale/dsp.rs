630aef241e1c0295a7b8785425f430891aa0aa12
Luis Vega <vegaluisjose@gmail.com>
2021-07-04 17:51:18 +0000

    [tools][prim] rename DspParam

--- src/tools/prim/src/ultrascale/dsp.rs
+++ src/tools/prim/src/ultrascale/dsp.rs
@@ -1,16 +1,16 @@
 use crate::{ParamSet, PortSet, ToPrim};
 
 #[derive(Clone, Debug, Default, PartialEq)]
-pub struct DspParam;
+pub struct ParamValue;
 
 #[derive(Clone, Debug, Default)]
 pub struct Dsp;
 
-impl ToPrim<DspParam> for Dsp {
+impl ToPrim<ParamValue> for Dsp {
     fn to_name(&self) -> String {
         String::from("DSP48E2")
     }
-    fn to_param(&self) -> ParamSet<DspParam> {
+    fn to_param(&self) -> ParamSet<ParamValue> {
         ParamSet::new()
     }
     fn to_input(&self) -> PortSet {
