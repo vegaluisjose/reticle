0d59195f158417ca4141e68e09440da31d6204a7
Luis Vega <vegaluisjose@gmail.com>
2021-07-13 15:35:56 +0000

    [tools][xpand] test name fix

--- src/tools/xpand/tests/xpand.rs
+++ src/tools/xpand/tests/xpand.rs
@@ -5,14 +5,14 @@ use xpand::errors::Error;
 use xpand::try_from_xir_prog;
 
 fn test(name: &str) -> Result<(), Error> {
-    let mut i = Path::new("../../../examples/xir").join(name);
-    let mut o = Path::new("../../../examples/struct").join(name);
-    i.set_extension("xir");
-    o.set_extension("v");
-    let p = Parser::parse_from_file(i)?;
-    let e = read_to_string(o);
-    let r = try_from_xir_prog(&p)?;
-    assert_eq!(r.to_string(), e);
+    let mut input = Path::new("../../../examples/xir").join(name);
+    let mut output = Path::new("../../../examples/struct").join(name);
+    input.set_extension("xir");
+    output.set_extension("v");
+    let parsed = Parser::parse_from_file(input)?;
+    let exp = read_to_string(output);
+    let res = try_from_xir_prog(&parsed)?;
+    assert_eq!(res.to_string(), exp);
     Ok(())
 }
 
