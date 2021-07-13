c19a1a669e2552e367282a62ecc501c47f341e2b
Luis Vega <vegaluisjose@gmail.com>
2021-07-13 16:12:02 +0000

    [tools][xpand] bypass clippy for ToDecl trait

--- src/tools/xpand/src/decl.rs
+++ src/tools/xpand/src/decl.rs
@@ -1,5 +1,9 @@
 use verilog::ast as vl;
 
+// TODO: this will be eventually be replaced with
+// traits in to_verilog.rs file. Therefore, we can
+// bypass clippy checks here.
+#[allow(clippy::module_name_repetitions)]
 pub trait ToDecl {
     fn to_decl(&self) -> vl::Decl;
 }
